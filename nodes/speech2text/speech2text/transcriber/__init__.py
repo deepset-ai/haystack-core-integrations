03f2c8979140b4183e10cbf083071d4a0c2aa56b
Sara Zan <sara.zanzottera@deepset.ai>
2022-10-21 14:56:55 +0200

    Update __init__.py

--- nodes/speech2text/speech2text/transcriber/__init__.py
+++ nodes/speech2text/speech2text/transcriber/__init__.py
@@ -1,2 +1,3 @@
 from speech2text.transcriber.base import BaseSpeechTranscriber
 from speech2text.transcriber.wav2vec import Wav2VecTranscriber
+from speech2text.transcriber.whisper import WhisperTranscriber
