f308e2f4f98c274b9c001953f0ccdef914b3b26c
Massimiliano Pippi <mpippi@gmail.com>
2023-03-09 08:49:45 +0100

    boh

--- stores/mongodb-documentstore/tests/test_documentstore.py
+++ stores/mongodb-documentstore/tests/test_documentstore.py
@@ -1,6 +1,14 @@
+import pytest
+
+from haystack.testing import DocumentStoreBaseTestAbstract
+
 from mongodb_documentstore import MongoDBDocumentStore
 
 
-class TestMongoDBDocumentStore:
+class TestMongoDBDocumentStore(DocumentStoreBaseTestAbstract):
+    @pytest.fixture
+    def ds(self, tmp_path):
+        return MongoDBDocumentStore()
+
     def test_init(self):
         MongoDBDocumentStore()
