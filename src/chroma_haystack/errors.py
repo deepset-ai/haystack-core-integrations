e389b487031714e3dfcbe9171e00c98aa2c719db
Massimiliano Pippi <mpippi@gmail.com>
2023-08-12 09:48:10 +0200

    make linter happy

--- src/chroma_haystack/errors.py
+++ src/chroma_haystack/errors.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present John Doe <jd@example.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-from haystack.preview.document_stores.errors import FilterError, DocumentStoreError
+from haystack.preview.document_stores.errors import DocumentStoreError, FilterError
 
 
 class ChromaDocumentStoreError(DocumentStoreError):
