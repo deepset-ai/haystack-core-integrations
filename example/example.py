3b47efd7676fc6e58e3f3d82aea6045024356316
Massimiliano Pippi <mpippi@gmail.com>
2023-08-18 18:08:17 +0200

    fix example code

--- example/example.py
+++ example/example.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 
 from haystack.preview import Pipeline
-from haystack.preview.components import TextFileToDocument, DocumentWriter
+from haystack.preview.components.file_converters import TextFileToDocument
+from haystack.preview.components.writers import DocumentWriter
 
 from chroma_haystack import ChromaDocumentStore
 from chroma_haystack.retriever import ChromaDenseRetriever
@@ -10,18 +11,17 @@ from chroma_haystack.retriever import ChromaDenseRetriever
 HERE = Path(__file__).resolve().parent
 file_paths = [HERE / "data" / Path(name) for name in os.listdir("data")]
 
+# Chroma is used in-memory so we use the same instances in the two pipelines below
 document_store = ChromaDocumentStore()
 
 indexing = Pipeline()
-indexing.add_document_store("chroma", document_store)
 indexing.add_component("converter", TextFileToDocument())
-indexing.add_component("writer", DocumentWriter(), document_store="chroma")
+indexing.add_component("writer", DocumentWriter(document_store))
 indexing.connect("converter", "writer")
 indexing.run({"converter": {"paths": file_paths}})
 
 querying = Pipeline()
-querying.add_document_store("chroma", document_store)
-querying.add_component("retriever", ChromaDenseRetriever(), document_store="chroma")
+querying.add_component("retriever", ChromaDenseRetriever(document_store))
 results = querying.run({"retriever": {"queries": ["Variable declarations"], "top_k": 3}})
 
 for d in results["retriever"][0]:
