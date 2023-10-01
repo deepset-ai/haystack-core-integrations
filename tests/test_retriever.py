53ea9b61c8a44ce09d5e807db531060dd1ce8ad8
Massimiliano Pippi <mpippi@gmail.com>
2023-10-01 09:54:33 +0200

    support single query socket

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -1,7 +1,7 @@
 import pytest
 
 from chroma_haystack.document_store import ChromaDocumentStore
-from chroma_haystack.retriever import ChromaDenseRetriever
+from chroma_haystack.retriever import ChromaQueryRetriever
 
 
 @pytest.mark.integration
@@ -9,9 +9,9 @@ def test_retriever_to_json(request):
     ds = ChromaDocumentStore(
         collection_name=request.node.name, embedding_function="HuggingFaceEmbeddingFunction", api_key="1234567890"
     )
-    retriever = ChromaDenseRetriever(ds, filters={"foo": "bar"}, top_k=99)
+    retriever = ChromaQueryRetriever(ds, filters={"foo": "bar"}, top_k=99)
     assert retriever.to_dict() == {
-        "type": "ChromaDenseRetriever",
+        "type": "ChromaQueryRetriever",
         "init_parameters": {
             "filters": {"foo": "bar"},
             "top_k": 99,
@@ -27,7 +27,7 @@ def test_retriever_to_json(request):
 @pytest.mark.integration
 def test_retriever_from_json(request):
     data = {
-        "type": "ChromaDenseRetriever",
+        "type": "ChromaQueryRetriever",
         "init_parameters": {
             "filters": {"bar": "baz"},
             "top_k": 42,
@@ -38,7 +38,7 @@ def test_retriever_from_json(request):
             },
         },
     }
-    retriever = ChromaDenseRetriever.from_dict(data)
+    retriever = ChromaQueryRetriever.from_dict(data)
     assert retriever.document_store._collection_name == request.node.name
     assert retriever.document_store._embedding_function == "HuggingFaceEmbeddingFunction"
     assert retriever.document_store._embedding_function_params == {"api_key": "1234567890"}
