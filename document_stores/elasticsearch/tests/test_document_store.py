99b80cdd85fd9994f627afc1d49584fc8ec776b7
anakin87 <stefanofiorucci@gmail.com>
2023-11-20 12:15:29 +0100

    move ruff ignore error

--- document_stores/elasticsearch/tests/test_document_store.py
+++ document_stores/elasticsearch/tests/test_document_store.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# ruff: noqa: S311
-
 import random
 from typing import List
 from unittest.mock import patch
@@ -319,7 +317,10 @@ class TestDocumentStore(DocumentStoreBaseTests):
         Test that handling of pagination works as expected, when the matching documents are > 10.
         """
 
-        docs = [Document(content=f"Document {i}", embedding=[random.random() for _ in range(4)]) for i in range(20)]
+        docs = [
+            Document(content=f"Document {i}", embedding=[random.random() for _ in range(4)])  # noqa: S311
+            for i in range(20)
+        ]
 
         docstore.write_documents(docs)
         results = docstore._embedding_retrieval(query_embedding=[0.1, 0.1, 0.1, 0.1], top_k=11, filters={})
