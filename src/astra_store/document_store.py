85246f1db9783d4e4ab220e39edd19e8d120def8
ElenaKusevska <elena.kusevska@gmail.com>
2023-11-22 20:24:55 +0100

    implement write_documents

--- src/astra_store/document_store.py
+++ src/astra_store/document_store.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import logging
-from typing import Any, Dict, List, Optional, Literal
+from typing import Any, Dict, List, Optional
 from pydantic import validate_arguments
 
 from haystack.preview.dataclasses import Document
@@ -37,7 +37,6 @@ class AstraDocumentStore:
         astra_keyspace: str,
         astra_collection: str,
         embedding_dim: int,
-        duplicate_documents: Literal["skip", "overwrite", "fail"],
         similarity: str = "cosine",
     ):
         """
@@ -67,7 +66,6 @@ class AstraDocumentStore:
         self.astra_collection = astra_collection
         self.embedding_dim = embedding_dim
         self.similarity = similarity
-        self.duplicate_documents = duplicate_documents
 
         self.index = AstraClient(
             astra_id = self.astra_id,
@@ -79,6 +77,102 @@ class AstraDocumentStore:
             similarity_function = self.similarity
         )
         self.batch_size = 2
+
+
+    def write_documents(
+        self,
+        documents: Union[List[dict], List[Document]],
+        index: Optional[str] = None,
+        batch_size: int = 20,
+        policy: DuplicatePolicy = DuplicatePolicy.FAIL
+    ):
+        """
+        Indexes documents for later queries.
+
+        :param documents: a list of Python dictionaries or a list of Haystack Document objects.
+                          For documents as dictionaries, the format is {"text": "<the-actual-text>"}.
+                          Optionally: Include meta data via {"text": "<the-actual-text>",
+                          "meta":{"name": "<some-document-name>, "author": "somebody", ...}}
+                          It can be used for filtering and is accessible in the responses of the Finder.
+        :param index: Optional name of index where the documents shall be written to.
+                      If None, the DocumentStore's default index (self.index) will be used.
+        :param batch_size: Number of documents that are passed to bulk function at a time.
+        :param policy:  Handle duplicate documents based on DuplicatePolicy parameter options.
+                        Parameter options : ( 'skip','overwrite','fail')
+                        skip: Ignore the duplicates documents
+                        overwrite: Update any existing documents with the same ID when adding documents.
+                        fail: an error is raised if the document ID of the document being added already
+                        exists.
+        :param headers: Custom HTTP headers to pass to document store client if supported (e.g. {'Authorization': 'Basic YWRtaW46cm9vdA=='} for basic authentication)
+
+        :return: None
+        """
+
+        if index == None:
+            index = self.index 
+
+        if batch_size > 20:
+            logger.warning(f"batch_size set to {batch_size}, but maximum batch_size for Astra when using the JSON API is 20. batch_size set to 20.")
+            batch_size = 20
+
+        def _convert_input_document(document: Union[dict, Document]):
+            if not isinstance(document, dict):
+                data = asdict(document)
+            meta = data.pop("meta")
+            document_dict = {**data, **meta}
+            document_dict["_id"] = document_dict.pop("id")
+            if "embedding" in document_dict:
+                document_dict["$vector"] = list(document_dict.pop("embedding"))
+            else:
+                document_dict["$vector"] = [0]*self.embedding_dim
+
+            return document_dict
+
+        documents_to_write = [_convert_input_document(doc) for doc in documents]
+
+        duplicate_documents = []
+        i = 0
+        while i < len(documents_to_write):
+            doc = documents_to_write[i]
+            id_exists = self.index.find_document(find_key="_id", find_value=doc["_id"])["exists"]
+            if id_exists: 
+                duplicate_documents.append(doc)
+                del documents_to_write[i]
+                i = i - 1
+            i = i + 1
+
+        def _batches(inputlist, batchsize):
+            print(batchsize)
+            l = len(inputlist)
+            for ndx in range(0, l, batchsize):
+                yield inputlist[ndx:min(ndx + batchsize, l)]
+
+        if policy == DuplicatePolicy.SKIP:
+            if len(documents_to_write) > 0:
+                for batch in _batches(documents_to_write, batch_size):
+                    inserted_ids = index.insert(batch, "_id")
+                    logger.info(f"write_documents inserted documents with id {inserted_ids}")
+            else:
+                logger.warning("No new documents to write to astra. No documents written. Argument policy set to SKIP")
+        
+        elif policy == DuplicatePolicy.OVERWRITE:
+            if len(documents_to_write) > 0:
+                for batch in _batches(documents_to_write, batch_size):
+                    inserted_ids = index.insert(batch, "_id")
+                    logger.info(f"write_documents inserted documents with id {inserted_ids}")
+            
+            if len(duplicate_documents) > 0:
+                updated_ids = []
+                for duplicate_doc in duplicate_documents:
+                    updated = index.update_document(duplicate_doc, "_id")
+                    if updated:
+                        updated_ids.append(duplicate_doc["_id"])
+                logger.info(f"write_documents updated documents with id {updated_ids}")
+        
+        elif policy == DuplicatePolicy.FAIL:
+            raise Exception(f"write documents called with duplicate ids {[x['_id'] for x in documents_to_write]}, but argument policy set to FAIL")
+
+
     def count_documents(self) -> int:
         """
         Returns how many documents are present in the document store.
