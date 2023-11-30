eeadea651774c0c102611d07858547f240897060
tstadel <thomas.stadelmann@deepset.ai>
2023-11-30 13:53:07 +0100

    feat: add OpenSearchDocumentStore

---
+++ integrations/opensearch/src/opensearch_haystack/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present Silvano Cerza <silvanocerza@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+from opensearch_haystack.document_store import OpenSearchDocumentStore
+
+__all__ = ["OpenSearchDocumentStore"]
