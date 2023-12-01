59e2ab0cdf1abf22d5ac37af99f535c1a9a308ea
Mouna Tayahi <mouna_tayahi@ext.carrefour.com>
2023-12-01 10:21:41 +0100

    Skip tests with unsupported filter

--- tests/test_document_store.py
+++ tests/test_document_store.py
@@ -2,8 +2,10 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import os
+from typing import List
 
 import pytest
+from haystack.preview import Document
 from haystack.preview.document_stores import DuplicatePolicy
 from haystack.preview.testing.document_store import DocumentStoreBaseTests
 
@@ -47,3 +49,118 @@ class TestDocumentStore(DocumentStoreBaseTests):
         """
         docstore.delete_documents(delete_all=True)
         assert docstore.count_documents() == 0
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lte")
+    @pytest.mark.unit
+    def test_lte_filter_non_numeric(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $ne.")
+    @pytest.mark.unit
+    def test_ne_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gte.")
+    @pytest.mark.unit
+    def test_gte_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gte.")
+    @pytest.mark.unit
+    def test_gte_filter_non_numeric(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gte.")
+    @pytest.mark.unit
+    def test_gte_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gte.")
+    @pytest.mark.unit
+    def test_gte_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gt.")
+    @pytest.mark.unit
+    def test_gt_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gt.")
+    @pytest.mark.unit
+    def test_gt_filter_non_numeric(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gt.")
+    @pytest.mark.unit
+    def test_gt_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $gt.")
+    @pytest.mark.unit
+    def test_gt_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lt.")
+    @pytest.mark.unit
+    def test_lt_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lt.")
+    @pytest.mark.unit
+    def test_lt_filter_non_numeric(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lt.")
+    @pytest.mark.unit
+    def test_lt_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lt.")
+    @pytest.mark.unit
+    def test_lt_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lte.")
+    @pytest.mark.unit
+    def test_lte_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lte.")
+    @pytest.mark.unit
+    def test_lte_filter_non_numeric(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lte.")
+    @pytest.mark.unit
+    def test_lte_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $lte.")
+    @pytest.mark.unit
+    def test_lte_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $ne.")
+    @pytest.mark.unit
+    def test_ne_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $ne.")
+    @pytest.mark.unit
+    def test_ne_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $nin.")
+    @pytest.mark.unit
+    def test_nin_filter(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $nin.")
+    @pytest.mark.unit
+    def test_nin_filter_table(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
+
+    @pytest.mark.skip(reason="Unsupported filter operator $nin.")
+    @pytest.mark.unit
+    def test_nin_filter_embedding(self, docstore: AstraDocumentStore, filterable_docs: List[Document]):
+        pass
