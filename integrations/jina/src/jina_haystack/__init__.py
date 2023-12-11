6a9de9a340a60a159d1f8203efc74005a72a05d1
Joan Fontanals <jfontanalsmartinez@gmail.com>
2023-12-11 17:57:50 +0100

    feat: add Jina Embeddings integration (#93)
    * feat: add Jina Embeddings integration
    
    * try to fix problems with missing imports
    
    * refactor: apply suggestions from code review
    
    Co-authored-by: Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
    
    * refactor: fix comments
    
    * Update document_embedder.py
    
    * Update text_embedder.py
    
    ---------
    
    Co-authored-by: Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>

---
+++ integrations/jina/src/jina_haystack/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from jina_haystack.document_embedder import JinaDocumentEmbedder
+from jina_haystack.text_embedder import JinaTextEmbedder
+
+__all__ = ["JinaDocumentEmbedder", "JinaTextEmbedder"]
