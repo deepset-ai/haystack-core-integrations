7e47dff250361cf5546bb22d4cbcec09782af569
Ashwin Mathur <97467100+awinml@users.noreply.github.com>
2023-12-11 17:28:58 +0530

    feat: Add support for V3 Embed models to CohereEmbedders (#89)
    * Add v3 embed models support
    
    * fix model names
    
    * Update integrations/cohere/src/cohere_haystack/embedders/document_embedder.py
    
    Co-authored-by: Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
    
    * Remove Optional type for input_type parameter
    
    * Apply docstring suggestions from code review
    
    Co-authored-by: Daria Fokina <daria.f93@gmail.com>
    
    ---------
    
    Co-authored-by: Stefano Fiorucci <44616784+anakin87@users.noreply.github.com>
    Co-authored-by: Daria Fokina <daria.f93@gmail.com>

--- integrations/cohere/src/cohere_haystack/embedders/document_embedder.py
+++ integrations/cohere/src/cohere_haystack/embedders/document_embedder.py
@@ -16,12 +16,28 @@ class CohereDocumentEmbedder:
     """
     A component for computing Document embeddings using Cohere models.
     The embedding of each Document is stored in the `embedding` field of the Document.
+
+    Usage Example:
+    ```python
+    from haystack import Document
+    from cohere_haystack.embedders.document_embedder import CohereDocumentEmbedder
+
+    doc = Document(content="I love pizza!")
+
+    document_embedder = CohereDocumentEmbedder()
+
+    result = document_embedder.run([doc])
+    print(result['documents'][0].embedding)
+
+    # [-0.453125, 1.2236328, 2.0058594, ...]
+    ```
     """
 
     def __init__(
         self,
         api_key: Optional[str] = None,
         model_name: str = "embed-english-v2.0",
+        input_type: str = "search_document",
         api_base_url: str = COHERE_API_URL,
         truncate: str = "END",
         use_async_client: bool = False,
@@ -37,9 +53,15 @@ class CohereDocumentEmbedder:
 
         :param api_key: The Cohere API key. It can be explicitly provided or automatically read from the environment
             variable COHERE_API_KEY (recommended).
-        :param model_name: The name of the model to use, defaults to `"embed-english-v2.0"`. Supported Models are
-            `"embed-english-v2.0"`/ `"large"`, `"embed-english-light-v2.0"`/ `"small"`,
-            `"embed-multilingual-v2.0"`/ `"multilingual-22-12"`.
+        :param model_name: The name of the model to use, defaults to `"embed-english-v2.0"`. Supported Models are:
+            `"embed-english-v3.0"`, `"embed-english-light-v3.0"`, `"embed-multilingual-v3.0"`,
+            `"embed-multilingual-light-v3.0"`, `"embed-english-v2.0"`, `"embed-english-light-v2.0"`,
+            `"embed-multilingual-v2.0"`. This list of all supported models can be found in the
+            [model documentation](https://docs.cohere.com/docs/models#representation).
+        :param input_type: Specifies the type of input you're giving to the model. Supported values are
+        "search_document", "search_query", "classification" and "clustering". Defaults to "search_document". Not
+        required for older versions of the embedding models (meaning anything lower than v3), but is required for more
+        recent versions (meaning anything bigger than v2).
         :param api_base_url: The Cohere API Base url, defaults to `https://api.cohere.ai/v1/embed`.
         :param truncate: Truncate embeddings that are too long from start or end, ("NONE"|"START"|"END"), defaults to
             `"END"`. Passing START will discard the start of the input. END will discard the end of the input. In both
@@ -68,6 +90,7 @@ class CohereDocumentEmbedder:
 
         self.api_key = api_key
         self.model_name = model_name
+        self.input_type = input_type
         self.api_base_url = api_base_url
         self.truncate = truncate
         self.use_async_client = use_async_client
@@ -85,6 +108,7 @@ class CohereDocumentEmbedder:
         return default_to_dict(
             self,
             model_name=self.model_name,
+            input_type=self.input_type,
             api_base_url=self.api_base_url,
             truncate=self.truncate,
             use_async_client=self.use_async_client,
@@ -137,14 +161,20 @@ class CohereDocumentEmbedder:
                 self.api_key, api_url=self.api_base_url, max_retries=self.max_retries, timeout=self.timeout
             )
             all_embeddings, metadata = asyncio.run(
-                get_async_response(cohere_client, texts_to_embed, self.model_name, self.truncate)
+                get_async_response(cohere_client, texts_to_embed, self.model_name, self.input_type, self.truncate)
             )
         else:
             cohere_client = Client(
                 self.api_key, api_url=self.api_base_url, max_retries=self.max_retries, timeout=self.timeout
             )
             all_embeddings, metadata = get_response(
-                cohere_client, texts_to_embed, self.model_name, self.truncate, self.batch_size, self.progress_bar
+                cohere_client,
+                texts_to_embed,
+                self.model_name,
+                self.input_type,
+                self.truncate,
+                self.batch_size,
+                self.progress_bar,
             )
 
         for doc, embeddings in zip(documents, all_embeddings):
