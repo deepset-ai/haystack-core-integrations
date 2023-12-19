b5b89e213e8bdc84dbc186b992a3c31274353df6
Obioma Anomnachi <onanomnachi@gmail.com>
2023-12-19 15:44:19 +0000

    pipeline examples lint fix

--- examples/pipeline_example.py
+++ examples/pipeline_example.py
@@ -90,6 +90,11 @@ rag_pipeline.draw("./rag_pipeline.png")
 # Run the pipeline
 question = "How many languages are there in the world today?"
 result = rag_pipeline.run(
-    {"embedder": {"text": question}, "retriever": {"top_k": 2}, "prompt_builder": {"question": question}, "answer_builder": {"query": question}}
+    {
+        "embedder": {"text": question},
+        "retriever": {"top_k": 2},
+        "prompt_builder": {"question": question},
+        "answer_builder": {"query": question},
+    }
 )
 print(result)
