82d2af3b02b295aa203435310391c3da1672925e
sahusiddharth <siddharth.sahu@plaksha.edu.in>
2023-12-20 15:43:03 +0530

    added top_k arguement in the run function of ElasticSearcBM25Retriever

--- integrations/elasticsearch/src/elasticsearch_haystack/bm25_retriever.py
+++ integrations/elasticsearch/src/elasticsearch_haystack/bm25_retriever.py
@@ -48,12 +48,12 @@ class ElasticsearchBM25Retriever:
         return default_from_dict(cls, data)
 
     @component.output_types(documents=List[Document])
-    def run(self, query: str):
+    def run(self, query: str, top_k: int=None):
         docs = self._document_store._bm25_retrieval(
             query=query,
             filters=self._filters,
             fuzziness=self._fuzziness,
-            top_k=self._top_k,
+            top_k=self._top_k if top_k == None else top_k,
             scale_score=self._scale_score,
         )
         return {"documents": docs}
