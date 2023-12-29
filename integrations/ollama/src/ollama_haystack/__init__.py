e30f18c1c84ed745de25450b2fd394431bcf1db9
Alistair Rogers <alistairlr112@gmail.com>
2023-12-29 12:59:57 +0000

    fix init paths

--- integrations/ollama/src/ollama_haystack/__init__.py
+++ integrations/ollama/src/ollama_haystack/__init__.py
@@ -2,6 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from integrations.ollama.src.ollama_haystack.generator import OllamaGenerator
+from ollama_haystack.generator import OllamaGenerator
 
 __all__ = ["OllamaGenerator"]
