c235f9655ab79693581c0b8a16bf1f5899f3c5a3
Alistair Rogers <alistairlr112@gmail.com>
2023-12-30 11:26:48 +0000

    correct type hint in output type

--- integrations/ollama/src/ollama_haystack/generator.py
+++ integrations/ollama/src/ollama_haystack/generator.py
@@ -84,7 +84,7 @@ class OllamaGenerator:
             },
         }
 
-    @component.output_types(replies=List[str], metadata=List[dict[str, Any]])
+    @component.output_types(replies=List[str], metadata=List[Dict[str, Any]])
     def run(self, prompt: str, generation_kwargs: Optional[Dict[str, Any]] = None):
         generation_kwargs = {**self.generation_kwargs, **(generation_kwargs or {})}
 
