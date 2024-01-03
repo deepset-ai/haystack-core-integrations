909f9279b3ebc2c344e4a7c3049f78e67f24a1f8
anakin87 <stefanofiorucci@gmail.com>
2024-01-03 18:17:51 +0100

    refinements

--- integrations/ollama/src/ollama_haystack/generator.py
+++ integrations/ollama/src/ollama_haystack/generator.py
@@ -5,25 +5,16 @@ from haystack import component
 from requests import Response
 
 
-def convert_to_haystack_response(ollama_response: Response) -> Dict[str, List[Any]]:
-    """
-    Convert a response from the Ollama API to the required Haystack format
-    :param ollama_response: A response (requests library) from the Ollama API
-    :return: A dictionary of the returned responses and metadata
-    """
-    json = ollama_response.json()
-
-    replies = json["response"]
-    meta = {key: value for key, value in json.items() if key != "response"}
-
-    return {"replies": [replies], "meta": [meta]}
-
-
 @component
 class OllamaGenerator:
+    """
+    Generator based on Ollama. Ollama is a library for easily running LLMs locally.
+    This component provides an interface to generate text using a LLM running in Ollama.
+    """
+
     def __init__(
         self,
-        model_name: str = "orca-mini",
+        model: str = "orca-mini",
         url: str = "http://localhost:11434/api/generate",
         generation_kwargs: Optional[Dict[str, Any]] = None,
         system_prompt: Optional[str] = None,
@@ -32,48 +23,57 @@ class OllamaGenerator:
         timeout: int = 30,
     ):
         """
-
-        :param model_name: The name of the LLM to use (from Ollama). Default is orca-mini
-        :param url: The URL to a running Ollama instance. Default is http://localhost:11434/api/generate
-        :param generation_kwargs: Optional arguments to pass to the Ollama generate function
-        :param system_prompt: Optional system message to (overrides what is defined in the Modelfile)
-        :param template: The full prompt or prompt template (overrides what is defined in the Modelfile)
+        :param model: The name of the model to use. The model should be available in the running Ollama instance.
+            Default is "orca-mini".
+        :param url: The URL of the generation endpoint of a running Ollama instance.
+            Default is "http://localhost:11434/api/generate".
+        :param generation_kwargs: Optional arguments to pass to the Ollama generation endpoint, such as temperature,
+            top_p, etc. See the
+            [Ollama docs](https://github.com/jmorganca/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values).
+        :param system_prompt: Optional system message (overrides what is defined in the Ollama Modelfile).
+        :param template: The full prompt template (overrides what is defined in the Ollama Modelfile).
         :param raw: If True, no formatting will be applied to the prompt. You may choose to use the raw parameter
-            if you are specifying a full templated prompt in your request to the API.\
-        :param timeout: The number of seconds before throwing a timeout error from the Ollama API. Default 3-
+            if you are specifying a full templated prompt in your request to the API.
+        :param timeout: The number of seconds before throwing a timeout error from the Ollama API.
+            Default is 30 seconds.
         """
         self.timeout = timeout
         self.raw = raw
         self.template = template
         self.system_prompt = system_prompt
-        self.model_name = model_name
+        self.model = model
         self.url = url
         self.generation_kwargs = generation_kwargs or {}
 
-    def _get_telemetry_data(self) -> Dict[str, str]:
-        """
-        Data that is sent to Posthog for usage analytics.
-        """
-        return {"model": self.model_name}
-
-    def _json_payload(self, prompt: str, generation_kwargs=None) -> Dict[str, Any]:
+    def _create_json_payload(self, prompt: str, generation_kwargs=None) -> Dict[str, Any]:
         """
         Returns A dictionary of JSON arguments for a POST request to an Ollama service
         :param prompt: the prompt to generate a response for
         :param generation_kwargs:
         :return: A dictionary of arguments for a POST request to an Ollama service
         """
-        if generation_kwargs is None:
-            generation_kwargs = {}
         return {
             "prompt": prompt,
-            "model": self.model_name,
+            "model": self.model,
             "stream": False,
             "raw": self.raw,
             "template": self.template,
             "system": self.system_prompt,
             "options": generation_kwargs,
         }
+
+    def _convert_to_haystack_response(self, ollama_response: Response) -> Dict[str, List[Any]]:
+        """
+        Convert a response from the Ollama API to the required Haystack format
+        :param ollama_response: A response (requests library) from the Ollama API
+        :return: A dictionary of the returned responses and metadata
+        """
+        resp_dict = ollama_response.json()
+
+        replies = [resp_dict["response"]]
+        meta = {key: value for key, value in resp_dict.items() if key != "response"}
+
+        return {"replies": replies, "meta": [meta]}
 
     @component.output_types(replies=List[str], metadata=List[Dict[str, Any]])
     def run(
@@ -82,19 +82,20 @@ class OllamaGenerator:
         generation_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """
-        Run an Ollama Model against a given prompt
-        :param prompt: The prompt to generate a response for
-        :param generation_kwargs: Additional model parameters listed in the documentation for the Ollama
-            Modelfile such as temperature
+        Run an Ollama Model on the a given prompt.
+        :param prompt: The prompt to generate a response for.
+        :param generation_kwargs: Optional arguments to pass to the Ollama generation endpoint, such as temperature,
+            top_p, etc. See the
+            [Ollama docs](https://github.com/jmorganca/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values).
         :return: A dictionary of the response and returned metadata
         """
         generation_kwargs = {**self.generation_kwargs, **(generation_kwargs or {})}
 
-        json_payload = self._json_payload(prompt, generation_kwargs)
+        json_payload = self._create_json_payload(prompt, generation_kwargs)
 
         response = requests.post(url=self.url, json=json_payload, timeout=self.timeout)
 
-        # Throw error on unsuccessful response
+        # throw error on unsuccessful response
         response.raise_for_status()
 
-        return convert_to_haystack_response(response)
+        return self._convert_to_haystack_response(response)
