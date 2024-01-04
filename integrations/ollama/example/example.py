1759da4f1fdd5e7f8c0a51b35753e8af6b05836b
Alistair Rogers <alistairlr112@gmail.com>
2024-01-04 14:26:02 +0000

    add comments on how to set up and expected output

--- integrations/ollama/example/example.py
+++ integrations/ollama/example/example.py
@@ -1,3 +1,10 @@
+# In order to run this example, you will need to have an instance of Ollama running with the
+# orca-mini model downloaded. We suggest you use the following commands to serve an orca-mini
+# model from Ollama
+#
+# docker run -d -p 11434:11434 --name ollama ollama/ollama:latest
+# docker exec ollama ollama pull orca-mini
+
 from haystack import Document, Pipeline
 from haystack.components.builders.prompt_builder import PromptBuilder
 from haystack.components.retrievers import InMemoryBM25Retriever
@@ -41,3 +48,8 @@ pipe.connect("prompt_builder", "llm")
 response = pipe.run({"prompt_builder": {"query": query}, "retriever": {"query": query}})
 
 print(response["llm"]["replies"])
+# An expected response - the output is not deterministic:
+# ['Based on the information provided, Super Mario is a successful military leader who fought
+# off several invasion attempts by his arch rival - Bowser. He is also an important politician and owns several
+# castles where he conducts political business. ' 'Therefore, it can be inferred that Super Mario is a combination of
+# both a military leader and an important politician.']
