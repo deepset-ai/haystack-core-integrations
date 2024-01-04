046cb08be64a23967a6af0b7566d3fff58fee1ff
Alistair Rogers <alistairlr112@gmail.com>
2024-01-04 10:55:21 +0000

    fix example with ruff

--- integrations/ollama/example/example.py
+++ integrations/ollama/example/example.py
@@ -1,14 +1,14 @@
 from haystack import Pipeline
+from haystack.components.builders.prompt_builder import PromptBuilder
 from haystack.components.retrievers import InMemoryBM25Retriever
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.components.builders.prompt_builder import PromptBuilder
 
 from ollama_haystack import OllamaGenerator
 
 template = """
 Given the following information, answer the question.
 
-Context: 
+Context:
 {% for document in documents %}
     {{ document.content }}
 {% endfor %}
@@ -20,7 +20,7 @@ document_store = InMemoryDocumentStore()
 
 pipe.add_component("retriever", InMemoryBM25Retriever(document_store=document_store))
 pipe.add_component("prompt_builder", PromptBuilder(template=template))
-pipe.add_component("llm", OllamaGenerator(model='orca-mini'))
+pipe.add_component("llm", OllamaGenerator(model="orca-mini"))
 pipe.connect("retriever", "prompt_builder.documents")
 pipe.connect("prompt_builder", "llm")
 
