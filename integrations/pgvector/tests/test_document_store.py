73254d8fa8026bdbee116b61f4ec69e81d2b3de6
anakin87 <stefanofiorucci@gmail.com>
2024-01-05 16:43:11 +0100

    add workflow for pgvector

--- integrations/pgvector/tests/test_document_store.py
+++ integrations/pgvector/tests/test_document_store.py
@@ -7,3 +7,17 @@ import numpy as np
 import pytest
 from haystack import Document
 from haystack.testing.document_store import CountDocumentsTest, DeleteDocumentsTest, WriteDocumentsTest
+
+
+from pgvector_haystack.document_store import PGvectorDocumentStore
+
+
+class TestDocumentStore(CountDocumentsTest, DeleteDocumentsTest, WriteDocumentsTest):
+
+    @pytest.fixture
+    def docstore(self) -> PGvectorDocumentStore:
+        """
+        This is the most basic requirement for the child class: provide
+        an instance of this document store so the base class can use it.
+        """
+        return PGvectorDocumentStore() # FIXME
