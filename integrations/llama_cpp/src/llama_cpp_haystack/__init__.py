a7f4cd7550774abe42644d2abcad3485da29c1a6
Ashwin Mathur <97467100+awinml@users.noreply.github.com>
2024-01-09 16:27:21 +0530

    feat: Add Llama.cpp Generator (#179)
    * Add llama-cpp-haystack
    
    * Fix links in README
    
    * Update README
    
    * Reorder integrations alphabetically
    
    * Fix mypy llama_cpp import

---
+++ integrations/llama_cpp/src/llama_cpp_haystack/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from llama_cpp_haystack.generator import LlamaCppGenerator
+
+__all__ = ["LlamaCppGenerator"]
