7f76bdf7efba3654272d8c87439837d688972fa2
Alistair Rogers <alistairlr112@gmail.com>
2024-01-14 15:36:11 +0000

    order imports in init

--- integrations/ollama/src/ollama_haystack/__init__.py
+++ integrations/ollama/src/ollama_haystack/__init__.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from ollama_haystack.chat.chat_generator import OllamaChatGenerator
-from ollama_haystack.generator import OllamaGenerator
 from ollama_haystack.embeddings.text_embedder import OllamaTextEmbedder
+from ollama_haystack.generator import OllamaGenerator
 
 __all__ = ["OllamaGenerator", "OllamaChatGenerator", "OllamaTextEmbedder"]
