7d5f795fee2c767233ce2de963b751e5c2d3c7f1
Massimiliano Pippi <mpippi@gmail.com>
2024-01-18 09:03:26 +0100

    update import paths for beta5 (#232)

--- integrations/gradient/tests/test_gradient_document_embedder.py
+++ integrations/gradient/tests/test_gradient_document_embedder.py
@@ -13,7 +13,6 @@ model = "bge-large"
 
 
 class TestGradientDocumentEmbedder:
-    @pytest.mark.unit
     def test_init_from_env(self, monkeypatch):
         monkeypatch.setenv("GRADIENT_ACCESS_TOKEN", access_token)
         monkeypatch.setenv("GRADIENT_WORKSPACE_ID", workspace_id)
@@ -23,28 +22,24 @@ class TestGradientDocumentEmbedder:
         assert embedder._gradient.workspace_id == workspace_id
         assert embedder._gradient._api_client.configuration.access_token == access_token
 
-    @pytest.mark.unit
     def test_init_without_access_token(self, monkeypatch):
         monkeypatch.delenv("GRADIENT_ACCESS_TOKEN", raising=False)
 
         with pytest.raises(ValueError):
             GradientDocumentEmbedder(workspace_id=workspace_id)
 
-    @pytest.mark.unit
     def test_init_without_workspace(self, monkeypatch):
         monkeypatch.delenv("GRADIENT_WORKSPACE_ID", raising=False)
 
         with pytest.raises(ValueError):
             GradientDocumentEmbedder(access_token=access_token)
 
-    @pytest.mark.unit
     def test_init_from_params(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         assert embedder is not None
         assert embedder._gradient.workspace_id == workspace_id
         assert embedder._gradient._api_client.configuration.access_token == access_token
 
-    @pytest.mark.unit
     def test_init_from_params_precedence(self, monkeypatch):
         monkeypatch.setenv("GRADIENT_ACCESS_TOKEN", "env_access_token")
         monkeypatch.setenv("GRADIENT_WORKSPACE_ID", "env_workspace_id")
@@ -54,7 +49,6 @@ class TestGradientDocumentEmbedder:
         assert embedder._gradient.workspace_id == workspace_id
         assert embedder._gradient._api_client.configuration.access_token == access_token
 
-    @pytest.mark.unit
     def test_to_dict(self):
         component = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         data = component.to_dict()
@@ -63,14 +57,12 @@ class TestGradientDocumentEmbedder:
             "init_parameters": {"workspace_id": workspace_id, "model_name": "bge-large"},
         }
 
-    @pytest.mark.unit
     def test_warmup(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         embedder._gradient.get_embeddings_model = MagicMock()
         embedder.warm_up()
         embedder._gradient.get_embeddings_model.assert_called_once_with(slug="bge-large")
 
-    @pytest.mark.unit
     def test_warmup_doesnt_reload(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         embedder._gradient.get_embeddings_model = MagicMock(default_return_value="fake model")
@@ -78,14 +70,12 @@ class TestGradientDocumentEmbedder:
         embedder.warm_up()
         embedder._gradient.get_embeddings_model.assert_called_once_with(slug="bge-large")
 
-    @pytest.mark.unit
     def test_run_fail_if_not_warmed_up(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
 
         with pytest.raises(RuntimeError, match="warm_up()"):
             embedder.run(documents=[Document(content=f"document number {i}") for i in range(5)])
 
-    @pytest.mark.unit
     def test_run(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         embedder._embedding_model = NonCallableMagicMock()
@@ -105,7 +95,6 @@ class TestGradientDocumentEmbedder:
             assert isinstance(doc.embedding, list)
             assert isinstance(doc.embedding[0], float)
 
-    @pytest.mark.unit
     def test_run_batch(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         embedder._embedding_model = NonCallableMagicMock()
@@ -126,7 +115,6 @@ class TestGradientDocumentEmbedder:
             assert isinstance(doc.embedding, list)
             assert isinstance(doc.embedding[0], float)
 
-    @pytest.mark.unit
     def test_run_custom_batch(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id, batch_size=20)
         embedder._embedding_model = NonCallableMagicMock()
@@ -148,7 +136,6 @@ class TestGradientDocumentEmbedder:
             assert isinstance(doc.embedding, list)
             assert isinstance(doc.embedding[0], float)
 
-    @pytest.mark.unit
     def test_run_empty(self):
         embedder = GradientDocumentEmbedder(access_token=access_token, workspace_id=workspace_id)
         embedder._embedding_model = NonCallableMagicMock()
