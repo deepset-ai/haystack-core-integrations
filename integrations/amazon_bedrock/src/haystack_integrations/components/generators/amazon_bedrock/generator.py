b6115c21282257984ebc0046e4013174a29c5f6e
Vladimir Blagojevic <dovlex@gmail.com>
2024-01-30 18:04:18 +0100

    chore: Adjust amazon bedrock helper classes names (#297)
    * Adjust amazon bedrock helper classes names
    
    * Linting
    
    * Update tests
    
    * More linting
    
    * Small update

--- integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/generator.py
+++ integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/generator.py
@@ -7,7 +7,7 @@ import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 from haystack import component, default_from_dict, default_to_dict
 
-from .amazon_bedrock_adapters import (
+from .adapters import (
     AI21LabsJurassic2Adapter,
     AmazonTitanAdapter,
     AnthropicClaudeAdapter,
@@ -15,15 +15,15 @@ from .amazon_bedrock_adapters import (
     CohereCommandAdapter,
     MetaLlama2ChatAdapter,
 )
-from .amazon_bedrock_handlers import (
-    DefaultPromptHandler,
-    DefaultTokenStreamingHandler,
-    TokenStreamingHandler,
-)
 from .errors import (
     AmazonBedrockConfigurationError,
     AmazonBedrockInferenceError,
     AWSConfigurationError,
+)
+from .handlers import (
+    DefaultPromptHandler,
+    DefaultTokenStreamingHandler,
+    TokenStreamingHandler,
 )
 
 logger = logging.getLogger(__name__)
