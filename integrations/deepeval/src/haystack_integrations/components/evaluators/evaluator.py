248dfb03f927651d109e6c172e89bd825dd8b2e3
shadeMe <shadeMe@users.noreply.github.com>
2024-02-05 15:22:30 +0100

    feat: Implement `DeepEvalEvaluator` and `DeepEvalMetrics`

---
+++ integrations/deepeval/src/haystack_integrations/components/evaluators/evaluator.py
@@ -0,0 +1,149 @@
+import json
+from typing import Any, Callable, Dict, List, Optional, Union
+
+from deepeval.evaluate import TestResult, evaluate
+from deepeval.metrics import BaseMetric
+from deepeval.test_case import LLMTestCase
+from haystack import DeserializationError, component, default_from_dict, default_to_dict
+
+from .metrics import (
+    METRIC_DESCRIPTORS,
+    DeepEvalMetric,
+    InputConverters,
+)
+
+
+@component
+class DeepEvalEvaluator:
+    """
+    A component that uses the DeepEval framework to evaluate inputs against a specific metric.
+
+    The supported metrics are defined by :class:`DeepEvalMetric`. The inputs of the component
+    metric-dependent.
+    """
+
+    _backend_metric: BaseMetric
+    # Wrapped for easy mocking.
+    _backend_callable: Callable[[List[LLMTestCase], BaseMetric], List[TestResult]]
+
+    def __init__(
+        self,
+        metric: Union[str, DeepEvalMetric],
+        metric_params: Optional[Dict[str, Any]] = None,
+    ):
+        """
+        Construct a new DeepEval evaluator.
+
+        :param metric:
+            The metric to use for evaluation.
+        :param metric_params:
+            Parameters to pass to the metric's constructor.
+        """
+        self.metric = metric if isinstance(metric, DeepEvalMetric) else DeepEvalMetric.from_str(metric)
+        self.metric_params = metric_params
+        self.descriptor = METRIC_DESCRIPTORS[self.metric]
+
+        self._init_backend()
+        expected_inputs = self.descriptor.input_parameters
+        component.set_input_types(self, **expected_inputs)
+
+    @component.output_types(results=List[List[Dict[str, Any]]])
+    def run(self, **inputs) -> Dict[str, Any]:
+        """
+        Run the DeepEval evaluator.
+
+        Example:
+        ```python
+        pipeline = Pipeline()
+        evaluator = DeepEvalEvaluator(
+            metric=DeepEvalMetric.ANSWER_RELEVANCY,
+            metric_params={"model": "gpt-4"},
+        )
+        pipeline.add_component("evaluator", evaluator)
+
+        # Each metric expects a specific set of parameters as input. Refer to the
+        # DeepEvalMetric class' documentation for more details.
+        output = pipeline.run({"evaluator": {
+            "questions": ["question],
+            "contexts": [["context"]],
+            "responses": ["response"]
+        }})
+        ```
+
+        :param inputs:
+            The inputs to evaluate. These are determined by the
+            metric being calculated. See :class:`DeepEvalMetric` for more
+            information.
+        :returns:
+            A nested list of metric results. Each input can have one or more
+            results, depending on the metric. Each result is a dictionary
+            containing the following keys and values:
+                * `name` - The name of the metric.
+                * `score` - The score of the metric.
+                * `explanation` - An optional explanation of the score.
+        """
+        InputConverters.validate_input_parameters(self.metric, self.descriptor.input_parameters, inputs)
+        converted_inputs: List[LLMTestCase] = list(self.descriptor.input_converter(**inputs))  # type: ignore
+
+        results = self._backend_callable(converted_inputs, self._backend_metric)
+        converted_results = [[result.to_dict() for result in self.descriptor.output_converter(x)] for x in results]
+
+        return {"results": converted_results}
+
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        Serialize this component to a dictionary.
+        """
+
+        def check_serializable(obj: Any):
+            try:
+                json.dumps(obj)
+                return True
+            except (TypeError, OverflowError):
+                return False
+
+        if not check_serializable(self.metric_params):
+            msg = "DeepEval evaluator cannot serialize the metric parameters"
+            raise DeserializationError(msg)
+
+        return default_to_dict(
+            self,
+            metric=self.metric,
+            metric_params=self.metric_params,
+        )
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "DeepEvalEvaluator":
+        """
+        Deserialize a component from a dictionary.
+
+        :param data:
+            The dictionary to deserialize from.
+        """
+        return default_from_dict(cls, data)
+
+    @staticmethod
+    def _invoke_deepeval(test_cases: List[LLMTestCase], metric: BaseMetric) -> List[TestResult]:
+        return evaluate(test_cases, [metric])
+
+    def _init_backend(self):
+        """
+        Initialize the DeepEval backend.
+        """
+        if self.descriptor.init_parameters is not None:
+            if self.metric_params is None:
+                msg = f"DeepEval metric '{self.metric}' expected init parameters but got none"
+                raise ValueError(msg)
+            elif not all(k in self.descriptor.init_parameters for k in self.metric_params.keys()):
+                msg = (
+                    f"Invalid init parameters for DeepEval metric '{self.metric}'. "
+                    f"Expected: {list(self.descriptor.init_parameters.keys())}"
+                )
+
+                raise ValueError(msg)
+        backend_metric_params = dict(self.metric_params) if self.metric_params is not None else {}
+
+        # This shouldn't matter at all as we aren't asserting the outputs, but just in case...
+        backend_metric_params["threshold"] = 0.0
+        self._backend_metric = self.descriptor.backend(**backend_metric_params)
+        self._backend_callable = DeepEvalEvaluator._invoke_deepeval
---
+++ integrations/deepeval/tests/test_evaluator.py
@@ -0,0 +1,314 @@
+import copy
+import os
+from dataclasses import dataclass
+from typing import Dict, Optional
+from unittest.mock import patch
+
+import pytest
+from haystack import DeserializationError
+
+from haystack_integrations.components.evaluators import DeepEvalEvaluator, DeepEvalMetric
+from deepeval.evaluate import TestResult, BaseMetric
+
+DEFAULT_QUESTIONS = [
+    "Which is the most popular global sport?",
+    "Who created the Python language?",
+]
+DEFAULT_CONTEXTS = [
+    [
+        "The popularity of sports can be measured in various ways, including TV viewership, social media presence, number of participants, and economic impact. Football is undoubtedly the world's most popular sport with major events like the FIFA World Cup and sports personalities like Ronaldo and Messi, drawing a followership of more than 4 billion people."
+    ],
+    [
+        "Python, created by Guido van Rossum in the late 1980s, is a high-level general-purpose programming language. Its design philosophy emphasizes code readability, and its language constructs aim to help programmers write clear, logical code for both small and large-scale software projects."
+    ],
+]
+DEFAULT_RESPONSES = [
+    "Football is the most popular sport with around 4 billion followers worldwide",
+    "Python language was created by Guido van Rossum.",
+]
+DEFAULT_GROUND_TRUTHS = [
+    "Football is the most popular sport with over 4 billion followers worldwide. It's horrible...",
+    "Python language was created by the seventh son of the seventh son.",
+]
+
+
+@dataclass(frozen=True)
+class Unserializable:
+    something: str
+
+
+@dataclass(frozen=True)
+class MockResult:
+    score: float
+    reason: Optional[str] = None
+    score_breakdown: Optional[Dict[str, float]] = None
+
+
+# Only returns results for the passed metrics.
+class MockBackend:
+    def __init__(self, metric: DeepEvalMetric) -> None:
+        self.metric = metric
+
+    def eval(self, test_cases, metric):
+        assert isinstance(metric, BaseMetric)
+
+        output_map = {
+            DeepEvalMetric.ANSWER_RELEVANCY: [MockResult(0.5, "1")],
+            DeepEvalMetric.FAITHFULNESS: [MockResult(0.1, "2")],
+            DeepEvalMetric.CONTEXTUAL_PRECISION: [MockResult(0.2, "3")],
+            DeepEvalMetric.CONTEXTUAL_RECALL: [MockResult(35, "4")],
+            DeepEvalMetric.CONTEXTUAL_RELEVANCE: [MockResult(1.5, "5")],
+        }
+
+        out = []
+        for x in test_cases:
+            r = TestResult(False, [], x.input, x.actual_output, x.expected_output, x.context, x.retrieval_context)
+            r.metrics = copy.deepcopy(output_map[self.metric])
+            out.append(r)
+        return out
+
+
+def test_evaluator_metric_init_params():
+    eval = DeepEvalEvaluator(DeepEvalMetric.ANSWER_RELEVANCY, metric_params={"model": "gpt-4-32k"})
+    assert eval._backend_metric.evaluation_model == "gpt-4-32k"
+
+    with pytest.raises(ValueError, match="Invalid init parameters"):
+        DeepEvalEvaluator(DeepEvalMetric.FAITHFULNESS, metric_params={"role": "village idiot"})
+
+    with pytest.raises(ValueError, match="expected init parameters"):
+        DeepEvalEvaluator(DeepEvalMetric.CONTEXTUAL_RECALL)
+
+
+def test_evaluator_serde():
+    init_params = {
+        "metric": DeepEvalMetric.ANSWER_RELEVANCY,
+        "metric_params": {"model": "gpt-4-32k"},
+    }
+    eval = DeepEvalEvaluator(**init_params)
+    serde_data = eval.to_dict()
+    new_eval = DeepEvalEvaluator.from_dict(serde_data)
+
+    assert eval.metric == new_eval.metric
+    assert eval.metric_params == new_eval.metric_params
+    assert type(new_eval._backend_metric) == type(eval._backend_metric)
+
+    with pytest.raises(DeserializationError, match=r"cannot serialize the metric parameters"):
+        eval.metric_params["model"] = Unserializable("")
+        eval.to_dict()
+
+
+@pytest.mark.parametrize(
+    "metric, inputs, params",
+    [
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": [], "contexts": [], "responses": []},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.FAITHFULNESS,
+            {"questions": [], "contexts": [], "responses": []},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_PRECISION,
+            {"questions": [], "contexts": [], "responses": [], "ground_truths": []},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RECALL,
+            {"questions": [], "contexts": [], "responses": [], "ground_truths": []},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RELEVANCE,
+            {"questions": [], "contexts": [], "responses": []},
+            {"model": "gpt-4"},
+        ),
+    ],
+)
+def test_evaluator_valid_inputs(metric, inputs, params):
+    init_params = {
+        "metric": metric,
+        "metric_params": params,
+    }
+    eval = DeepEvalEvaluator(**init_params)
+    output = eval.run(**inputs)
+
+
+@pytest.mark.parametrize(
+    "metric, inputs, error_string, params",
+    [
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": [], "contexts": [], "responses": []},
+            "expected init parameters but got none",
+            None,
+        ),
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": {}, "contexts": [], "responses": []},
+            "to be a collection of type 'list'",
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": [], "contexts": [], "responses": []},
+            "Invalid init parameters",
+            {"role": "chatbot"},
+        ),
+        (
+            DeepEvalMetric.FAITHFULNESS,
+            {"questions": [1], "contexts": [2], "responses": [3]},
+            "expects inputs to be of type 'str'",
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.FAITHFULNESS,
+            {"questions": [], "contexts": [[]], "responses": []},
+            "Mismatching counts ",
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RECALL,
+            {"questions": [], "contexts": [], "responses": []},
+            "expected input parameter ",
+            {"model": "gpt-4"},
+        ),
+    ],
+)
+def test_evaluator_invalid_inputs(metric, inputs, error_string, params):
+    with pytest.raises(ValueError, match=error_string):
+        init_params = {
+            "metric": metric,
+            "metric_params": params,
+        }
+        eval = DeepEvalEvaluator(**init_params)
+        output = eval.run(**inputs)
+
+
+# This test validates the expected outputs of the evaluator.
+# Each output is parameterized as a list of tuples, where each tuple is
+# (name, score, explanation). The name and explanation are optional. If
+# the name is None, then the metric name is used.
+@pytest.mark.parametrize(
+    "metric, inputs, expected_outputs, metric_params",
+    [
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            [[(None, 0.5, "1")]] * 2,
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.FAITHFULNESS,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            [[(None, 0.1, "2")]] * 2,
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_PRECISION,
+            {
+                "questions": DEFAULT_QUESTIONS,
+                "contexts": DEFAULT_CONTEXTS,
+                "responses": DEFAULT_RESPONSES,
+                "ground_truths": DEFAULT_GROUND_TRUTHS,
+            },
+            [[(None, 0.2, "3")]] * 2,
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RECALL,
+            {
+                "questions": DEFAULT_QUESTIONS,
+                "contexts": DEFAULT_CONTEXTS,
+                "responses": DEFAULT_RESPONSES,
+                "ground_truths": DEFAULT_GROUND_TRUTHS,
+            },
+            [[(None, 35, "4")]] * 2,
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RELEVANCE,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            [[(None, 1.5, "5")]] * 2,
+            {"model": "gpt-4"},
+        ),
+    ],
+)
+def test_evaluator_outputs(metric, inputs, expected_outputs, metric_params):
+    init_params = {
+        "metric": metric,
+        "metric_params": metric_params,
+    }
+    eval = DeepEvalEvaluator(**init_params)
+    eval._backend_callable = lambda testcases, metrics: MockBackend(metric).eval(testcases, metrics)
+    results = eval.run(**inputs)["results"]
+
+    assert type(results) == type(expected_outputs)
+    assert len(results) == len(expected_outputs)
+
+    for r, o in zip(results, expected_outputs):
+        assert len(r) == len(o)
+
+        expected = {(name if name is not None else str(metric), score, exp) for name, score, exp in o}
+        got = {(x["name"], x["score"], x["explanation"]) for x in r}
+        assert got == expected
+
+
+# This integration test validates the evaluator by running it against the
+# OpenAI API. It is parameterized by the metric, the inputs to the evalutor
+# and the metric parameters.
+@pytest.mark.skipif("OPENAI_API_KEY" not in os.environ, reason="OPENAI_API_KEY not set")
+@pytest.mark.parametrize(
+    "metric, inputs, metric_params",
+    [
+        (
+            DeepEvalMetric.ANSWER_RELEVANCY,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.FAITHFULNESS,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_PRECISION,
+            {
+                "questions": DEFAULT_QUESTIONS,
+                "contexts": DEFAULT_CONTEXTS,
+                "responses": DEFAULT_RESPONSES,
+                "ground_truths": DEFAULT_GROUND_TRUTHS,
+            },
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RECALL,
+            {
+                "questions": DEFAULT_QUESTIONS,
+                "contexts": DEFAULT_CONTEXTS,
+                "responses": DEFAULT_RESPONSES,
+                "ground_truths": DEFAULT_GROUND_TRUTHS,
+            },
+            {"model": "gpt-4"},
+        ),
+        (
+            DeepEvalMetric.CONTEXTUAL_RELEVANCE,
+            {"questions": DEFAULT_QUESTIONS, "contexts": DEFAULT_CONTEXTS, "responses": DEFAULT_RESPONSES},
+            {"model": "gpt-4"},
+        ),
+    ],
+)
+def test_integration_run(metric, inputs, metric_params):
+    init_params = {
+        "metric": metric,
+        "metric_params": metric_params,
+    }
+    eval = DeepEvalEvaluator(**init_params)
+    output = eval.run(**inputs)
+
+    assert type(output) == dict
+    assert len(output) == 1
+    assert "results" in output
+    assert len(output["results"]) == len(next(iter(inputs.values())))
