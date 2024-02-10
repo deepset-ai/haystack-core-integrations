4bbb169f10bcb98591d4e06d4f9996598b736a02
Stefano Fiorucci <stefanofiorucci@gmail.com>
2024-02-10 10:26:47 +0100

    make mypy happy

--- integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/embedding_backend/fastembed_backend.py
+++ integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/embedding_backend/fastembed_backend.py
@@ -8,7 +8,7 @@ class _FastembedEmbeddingBackendFactory:
     Factory class to create instances of fastembed embedding backends.
     """
 
-    _instances: ClassVar[Dict[str, "_FastembedEmbeddingBackendFactory"]] = {}
+    _instances: Dict[str, "_FastembedEmbeddingBackend"] = {}
 
     @staticmethod
     def get_embedding_backend(
