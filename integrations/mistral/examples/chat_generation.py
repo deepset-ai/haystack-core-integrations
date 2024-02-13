7c357a1aacd1e7b6db75b6d047e5fce8d1531cdd
Tuana Celik <tuana.celik@deepset.ai>
2024-02-14 00:50:58 +0100

    adding some examples

--- integrations/mistral/examples/chat_generation.py
+++
@@ -1,9 +0,0 @@
-from haystack_integrations.components.generators.mistral import MistralChatGenerator
-from haystack.dataclasses import ChatMessage
-from haystack.components.generators.utils import print_streaming_chunk
-
-messages = [ChatMessage.from_user("What's Natural Language Processing?")]
-
-client = MistralChatGenerator(model= "mistral-small", streaming_callback=print_streaming_chunk)
-response = client.run(messages)
-print(response)
