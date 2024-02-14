d6668d58189bde365365822eb55c9ce55b7472e4
David S. Batista <dsbatista@gmail.com>
2024-02-14 17:55:26 +0100

    cleaning

--- integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/generator.py
+++ integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/generator.py
@@ -73,21 +73,21 @@ class AmazonBedrockGenerator:
     def __init__(
         self,
         model: str,
-
-        aws_access_key_id: Optional[Secret] = EnvVarSecret(
-            env_vars=["AWS_ACCESS_KEY_ID"], strict=False).from_env_var("AWS_ACCESS_KEY_ID"),
-
+        aws_access_key_id: Optional[Secret] = EnvVarSecret(env_vars=["AWS_ACCESS_KEY_ID"], strict=False).from_env_var(
+            "AWS_ACCESS_KEY_ID"
+        ),
         aws_secret_access_key: Optional[Secret] = EnvVarSecret(
-            env_vars=["AWS_SECRET_ACCESS_KEY"], strict=False).from_env_var("AWS_SECRET_ACCESS_KEY"),
-
-        aws_session_token: Optional[Secret] = EnvVarSecret(
-            env_vars=["AWS_SESSION_TOKEN"], strict=False).from_env_var("AWS_SESSION_TOKEN"),
-
-        aws_region_name: Optional[Secret] = EnvVarSecret(
-            env_vars=["AWS_DEFAULT_REGION"], strict=False).from_env_var("AWS_DEFAULT_REGION"),
-
-        aws_profile_name: Optional[str] = EnvVarSecret(
-            env_vars=["AWS_PROFILE"], strict=False).from_env_var("AWS_PROFILE"),
+            env_vars=["AWS_SECRET_ACCESS_KEY"], strict=False
+        ).from_env_var("AWS_SECRET_ACCESS_KEY"),
+        aws_session_token: Optional[Secret] = EnvVarSecret(env_vars=["AWS_SESSION_TOKEN"], strict=False).from_env_var(
+            "AWS_SESSION_TOKEN"
+        ),
+        aws_region_name: Optional[Secret] = EnvVarSecret(env_vars=["AWS_DEFAULT_REGION"], strict=False).from_env_var(
+            "AWS_DEFAULT_REGION"
+        ),
+        aws_profile_name: Optional[str] = EnvVarSecret(env_vars=["AWS_PROFILE"], strict=False).from_env_var(
+            "AWS_PROFILE"
+        ),
         max_length: Optional[int] = 100,
         **kwargs,
     ):
@@ -98,7 +98,6 @@ class AmazonBedrockGenerator:
         self.max_length = max_length
 
         def resolve_secret(secret: Optional[Secret]) -> Optional[str]:
-            print(secret, secret.resolve_value())
             return secret.resolve_value() if secret else None
 
         try:
@@ -109,11 +108,6 @@ class AmazonBedrockGenerator:
                 aws_region_name=resolve_secret(aws_region_name),
                 aws_profile_name=resolve_secret(aws_profile_name),
             )
-
-            creds = session.get_credentials()
-            print("\n\n\n\nCreds")
-            print(creds.access_key, creds.secret_key, creds.token, creds.method, creds.expired, creds._refresh_needed)
-
             self.client = session.client("bedrock-runtime")
         except Exception as exception:
             msg = (
--- integrations/amazon_bedrock/tests/conftest.py
+++ integrations/amazon_bedrock/tests/conftest.py
@@ -1,5 +1,6 @@
-import pytest
 from unittest.mock import MagicMock, patch
+
+import pytest
 
 
 @pytest.fixture
