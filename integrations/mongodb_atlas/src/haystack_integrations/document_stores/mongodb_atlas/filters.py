9859b2eeffb2376e30d4299319e65316e9d7d223
ZanSara <sara.zanzottera@deepset.ai>
2024-02-15 16:29:55 +0100

    Update integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/filters.py

--- integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/filters.py
+++ integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/filters.py
@@ -1,5 +1,5 @@
 
-def haystack_filters_to_mongo(filters):
+def haystack_filters_to_mongo(filters: Optional[Dict[str, Any]]):
     # TODO
     if filters:
         raise "Filtering not yet implemented for MongoDBAtlasDocumentStore"
