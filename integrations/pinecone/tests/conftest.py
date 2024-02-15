6a127e619482068ba124e9985b2d8b2dccc06242
anakin87 <stefanofiorucci@gmail.com>
2024-02-15 15:23:18 +0100

    conftest - do not fail when namespace inexistent at removal

--- integrations/pinecone/tests/conftest.py
+++ integrations/pinecone/tests/conftest.py
@@ -3,6 +3,8 @@ import time
 import pytest
 from haystack.document_stores.types import DuplicatePolicy
 
+from pinecone.core.client.exceptions import NotFoundException
+
 from haystack_integrations.document_stores.pinecone import PineconeDocumentStore
 
 # This is the approximate time it takes for the documents to be available
@@ -51,4 +53,7 @@ def document_store(request):
     store.delete_documents = delete_documents_and_wait
 
     yield store
-    store._index.delete(delete_all=True, namespace=namespace)
+    try:
+        store._index.delete(delete_all=True, namespace=namespace)
+    except NotFoundException:
+        pass
