2f9450d6f6fdc8a7ea7df01c8e6745187478116d
David S. Batista <dsbatista@gmail.com>
2024-02-20 16:29:47 +0100

    liting

--- integrations/unstructured/tests/test_converter.py
+++ integrations/unstructured/tests/test_converter.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 import pytest
-from haystack.utils import Secret
 from haystack_integrations.components.converters.unstructured import UnstructuredFileConverter
 
 
