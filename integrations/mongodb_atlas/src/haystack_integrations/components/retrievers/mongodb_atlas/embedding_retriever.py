b80c4f85458cc163f0e26fa997dce02da0b78f1a
ZanSara <sara.zanzottera@deepset.ai>
2024-02-21 18:01:06 +0100

    black

--- integrations/mongodb_atlas/src/haystack_integrations/components/retrievers/mongodb_atlas/embedding_retriever.py
+++ integrations/mongodb_atlas/src/haystack_integrations/components/retrievers/mongodb_atlas/embedding_retriever.py
@@ -53,7 +53,7 @@ class MongoDBAtlasEmbeddingRetriever:
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "MongoDBAtlasEmbeddingRetriever":
         """
-        Deserializes a dictionary created with `MongoDBAtlasEmbeddingRetriever. to_dict()` into a 
+        Deserializes a dictionary created with `MongoDBAtlasEmbeddingRetriever. to_dict()` into a
         `MongoDBAtlasEmbeddingRetriever` instance.
 
         :param data: the dictionary returned by `MongoDBAtlasEmbeddingRetriever. to_dict()`
