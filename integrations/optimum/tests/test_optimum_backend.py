ad5a29039be7a68bb18eb1597da8e708ab6c793f
Ashwin Mathur <97467100+awinml@users.noreply.github.com>
2024-02-21 18:52:08 +0530

    feat: Add Optimum Embedders (#379)
    * Add Optimum Embedders
    
    * Add CI workflow
    
    * Update dependencies
    
    * Add embedding backend
    
    * Add pooling sub module; Update pyproject.toml with dependency info
    
    * Remove backend factory; Address review comments
    
    * Add API docs generation workflow
    
    * Add additional tests
    
    * Update order to 185 for API docs
    
    * Update integrations/optimum/pydoc/config.yml
    
    Co-authored-by: Daria Fokina <daria.f93@gmail.com>
    
    ---------
    
    Co-authored-by: Daria Fokina <daria.f93@gmail.com>

---
+++ integrations/optimum/tests/test_optimum_backend.py
@@ -0,0 +1,32 @@
+import pytest
+from haystack_integrations.components.embedders.optimum_backend import OptimumEmbeddingBackend
+from haystack_integrations.components.embedders.pooling import PoolingMode
+
+
+@pytest.fixture
+def backend():
+    model = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {"model_id": model}
+    backend = OptimumEmbeddingBackend(model=model, model_kwargs=model_kwargs, token=None)
+    return backend
+
+
+class TestOptimumBackend:
+    def test_embed_output_order(self, backend):
+        texts_to_embed = ["short text", "text that is longer than the other", "medium length text"]
+        embeddings = backend.embed(texts_to_embed, normalize_embeddings=False, pooling_mode=PoolingMode.MEAN)
+
+        # Compute individual embeddings in order
+        expected_embeddings = []
+        for text in texts_to_embed:
+            expected_embeddings.append(backend.embed(text, normalize_embeddings=False, pooling_mode=PoolingMode.MEAN))
+
+        # Assert that the embeddings are in the same order
+        assert embeddings == expected_embeddings
+
+    def test_run_pooling_modes(self, backend):
+        for pooling_mode in PoolingMode:
+            embedding = backend.embed("test text", normalize_embeddings=False, pooling_mode=pooling_mode)
+
+            assert len(embedding) == 768
+            assert all(isinstance(x, float) for x in embedding)
