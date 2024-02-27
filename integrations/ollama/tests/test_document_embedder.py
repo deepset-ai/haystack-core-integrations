99b50aa070b9d431e0349e6c7a42c07c51191bd8
Julian De Vita <julianmdevita@gmail.com>
2024-02-26 22:49:25 -0500

    Changed Embedding model to supported model: nomic-embed-text

--- integrations/ollama/tests/test_document_embedder.py
+++ integrations/ollama/tests/test_document_embedder.py
@@ -11,11 +11,11 @@ class TestOllamaDocumentEmbedder:
         assert embedder.timeout == 120
         assert embedder.generation_kwargs == {}
         assert embedder.url == "http://localhost:11434/api/embeddings"
-        assert embedder.model == "orca-mini"
+        assert embedder.model == "nomic-embed-text"
 
     def test_init(self):
         embedder = OllamaDocumentEmbedder(
-            model="orca-mini",
+            model="nomic-embed-text",
             url="http://my-custom-endpoint:11434/api/embeddings",
             generation_kwargs={"temperature": 0.5},
             timeout=3000,
@@ -24,7 +24,7 @@ class TestOllamaDocumentEmbedder:
         assert embedder.timeout == 3000
         assert embedder.generation_kwargs == {"temperature": 0.5}
         assert embedder.url == "http://my-custom-endpoint:11434/api/embeddings"
-        assert embedder.model == "orca-mini"
+        assert embedder.model == "nomic-embed-text"
 
     @pytest.mark.integration
     def test_model_not_found(self):
@@ -35,17 +35,17 @@ class TestOllamaDocumentEmbedder:
 
     @pytest.mark.integration
     def import_text_in_embedder(self):
-        embedder = OllamaDocumentEmbedder(model="orca-mini")
+        embedder = OllamaDocumentEmbedder(model="nomic-embed-text")
 
         with pytest.raises(TypeError):
             embedder.run("This is a text string. This should not work.")
 
     @pytest.mark.integration
     def test_run(self):
-        embedder = OllamaDocumentEmbedder(model="orca-mini")
+        embedder = OllamaDocumentEmbedder(model="nomic-embed-text")
         list_of_docs = [Document(content="This is a document containing some text.")]
         reply = embedder.run(list_of_docs)
 
         assert isinstance(reply, dict)
         assert all(isinstance(element, float) for element in reply["documents"][0].embedding)
-        assert reply["meta"]["model"] == "orca-mini"
+        assert reply["meta"]["model"] == "nomic-embed-text"
