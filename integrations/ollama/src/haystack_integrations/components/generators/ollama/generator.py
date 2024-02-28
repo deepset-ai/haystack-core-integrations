9f08e62d2d81f68b69b9f42ce91a8f8fa8060b05
Massimiliano Pippi <mpippi@gmail.com>
2024-02-28 18:15:14 +0100

    generators

--- integrations/ollama/src/haystack_integrations/components/generators/ollama/generator.py
+++ integrations/ollama/src/haystack_integrations/components/generators/ollama/generator.py
@@ -11,8 +11,21 @@ from requests import Response
 @component
 class OllamaGenerator:
     """
-    Generator based on Ollama. Ollama is a library for easily running LLMs locally.
-    This component provides an interface to generate text using a LLM running in Ollama.
+    Provides an interface to generate text using an LLM running on Ollama.
+
+    Usage example:
+    ```python
+    from haystack_integrations.components.generators.ollama import OllamaGenerator
+
+    generator = OllamaGenerator(model="zephyr",
+                                url = "http://localhost:11434/api/generate",
+                                generation_kwargs={
+                                "num_predict": 100,
+                                "temperature": 0.9,
+                                })
+
+    print(generator.run("Who is the best American actor?"))
+    ```
     """
 
     def __init__(
@@ -27,20 +40,25 @@ class OllamaGenerator:
         streaming_callback: Optional[Callable[[StreamingChunk], None]] = None,
     ):
         """
-        :param model: The name of the model to use. The model should be available in the running Ollama instance.
-            Default is "orca-mini".
-        :param url: The URL of the generation endpoint of a running Ollama instance.
-            Default is "http://localhost:11434/api/generate".
-        :param generation_kwargs: Optional arguments to pass to the Ollama generation endpoint, such as temperature,
+        :param model:
+            The name of the model to use. The model should be available in the running Ollama instance.
+        :param url:
+            The URL of the generation endpoint of a running Ollama instance.
+        :param generation_kwargs:
+            Optional arguments to pass to the Ollama generation endpoint, such as temperature,
             top_p, and others. See the available arguments in
             [Ollama docs](https://github.com/jmorganca/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values).
-        :param system_prompt: Optional system message (overrides what is defined in the Ollama Modelfile).
-        :param template: The full prompt template (overrides what is defined in the Ollama Modelfile).
-        :param raw: If True, no formatting will be applied to the prompt. You may choose to use the raw parameter
+        :param system_prompt:
+            Optional system message (overrides what is defined in the Ollama Modelfile).
+        :param template:
+            The full prompt template (overrides what is defined in the Ollama Modelfile).
+        :param raw:
+            If True, no formatting will be applied to the prompt. You may choose to use the raw parameter
             if you are specifying a full templated prompt in your API request.
-        :param timeout: The number of seconds before throwing a timeout error from the Ollama API.
-            Default is 120 seconds.
-        :param streaming_callback: A callback function that is called when a new token is received from the stream.
+        :param timeout:
+            The number of seconds before throwing a timeout error from the Ollama API.
+        :param streaming_callback:
+            A callback function that is called when a new token is received from the stream.
             The callback function accepts StreamingChunk as an argument.
         """
         self.timeout = timeout
@@ -54,8 +72,10 @@ class OllamaGenerator:
 
     def to_dict(self) -> Dict[str, Any]:
         """
-        Serialize this component to a dictionary.
-        :return: The serialized component as a dictionary.
+        Serializes the component to a dictionary.
+
+        :returns:
+              Dictionary with serialized data.
         """
         callback_name = serialize_callable(self.streaming_callback) if self.streaming_callback else None
         return default_to_dict(
@@ -73,9 +93,12 @@ class OllamaGenerator:
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "OllamaGenerator":
         """
-        Deserialize this component from a dictionary.
-        :param data: The dictionary representation of this component.
-        :return: The deserialized component instance.
+        Deserializes the component from a dictionary.
+
+        :param data:
+            Dictionary to deserialize from.
+        :returns:
+            Deserialized component.
         """
         init_params = data.get("init_parameters", {})
         serialized_callback_handler = init_params.get("streaming_callback")
@@ -86,11 +109,6 @@ class OllamaGenerator:
     def _create_json_payload(self, prompt: str, stream: bool, generation_kwargs=None) -> Dict[str, Any]:
         """
         Returns a dictionary of JSON arguments for a POST request to an Ollama service.
-        :param prompt: The prompt to generate a response for.
-        :param generation_kwargs: Optional arguments to pass to the Ollama generation endpoint, such as temperature,
-            top_p, and others. See the available arguments in
-            [Ollama docs](https://github.com/jmorganca/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values).
-        :return: A dictionary of arguments for a POST request to an Ollama service.
         """
         generation_kwargs = generation_kwargs or {}
         return {
@@ -105,9 +123,7 @@ class OllamaGenerator:
 
     def _convert_to_response(self, ollama_response: Response) -> Dict[str, List[Any]]:
         """
-        Convert a response from the Ollama API to the required Haystack format.
-        :param ollama_response: A response (requests library) from the Ollama API.
-        :return: A dictionary of the returned responses and metadata.
+        Converts a response from the Ollama API to the required Haystack format.
         """
 
         resp_dict = ollama_response.json()
@@ -119,9 +135,7 @@ class OllamaGenerator:
 
     def _convert_to_streaming_response(self, chunks: List[StreamingChunk]) -> Dict[str, List[Any]]:
         """
-        Convert a list of chunks response required Haystack format.
-        :param chunks: List of StreamingChunks
-        :return: A dictionary of the returned responses and metadata.
+        Converts a list of chunks response required Haystack format.
         """
 
         replies = ["".join([c.content for c in chunks])]
@@ -130,10 +144,8 @@ class OllamaGenerator:
         return {"replies": replies, "meta": [meta]}
 
     def _handle_streaming_response(self, response) -> List[StreamingChunk]:
-        """Handles Streaming response case
-
-        :param response: streaming response from ollama api.
-        :return: The List[StreamingChunk].
+        """
+        Handles Streaming response cases
         """
         chunks: List[StreamingChunk] = []
         for chunk in response.iter_lines():
@@ -146,8 +158,6 @@ class OllamaGenerator:
     def _build_chunk(self, chunk_response: Any) -> StreamingChunk:
         """
         Converts the response from the Ollama API to a StreamingChunk.
-        :param chunk: The chunk returned by the Ollama API.
-        :return: The StreamingChunk.
         """
         decoded_chunk = json.loads(chunk_response.decode("utf-8"))
 
@@ -164,12 +174,17 @@ class OllamaGenerator:
         generation_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """
-        Run an Ollama Model on the given prompt.
-        :param prompt: The prompt to generate a response for.
-        :param generation_kwargs: Optional arguments to pass to the Ollama generation endpoint, such as temperature,
+        Runs an Ollama Model on the given prompt.
+
+        :param prompt:
+            The prompt to generate a response for.
+        :param generation_kwargs:
+            Optional arguments to pass to the Ollama generation endpoint, such as temperature,
             top_p, and others. See the available arguments in
             [Ollama docs](https://github.com/jmorganca/ollama/blob/main/docs/modelfile.md#valid-parameters-and-values).
-        :return: A dictionary of the response and returned metadata
+        :returns: A dictionary with the following keys:
+            - `replies`: The responses from the model
+            - `meta`: The metadata collected during the run
         """
         generation_kwargs = {**self.generation_kwargs, **(generation_kwargs or {})}
 
