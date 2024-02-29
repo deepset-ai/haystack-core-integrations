a6ff1ce7596ebac86d5286f8e358368200ea3d57
Tobias Wochinger <tobias.wochinger@deepset.ai>
2024-02-29 10:21:16 +0100

    docs: review chroma integration

--- integrations/chroma/src/haystack_integrations/document_stores/chroma/errors.py
+++ integrations/chroma/src/haystack_integrations/document_stores/chroma/errors.py
@@ -6,12 +6,18 @@ from haystack.errors import FilterError
 
 
 class ChromaDocumentStoreError(DocumentStoreError):
+    """Parent class for all ChromaDocumentStore exceptions."""
+
     pass
 
 
 class ChromaDocumentStoreFilterError(FilterError, ValueError):
+    """Raised when a filter is not valid for a ChromaDocumentStore."""
+
     pass
 
 
 class ChromaDocumentStoreConfigError(ChromaDocumentStoreError):
+    """Raised when a configuration is not valid for a ChromaDocumentStore."""
+
     pass
