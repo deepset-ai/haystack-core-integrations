3d63cd3170ba41fc70202539d938ab60d405a726
anakin87 <stefanofiorucci@gmail.com>
2024-03-01 12:01:51 +0100

    llama.cpp - review docstrings

--- integrations/llama_cpp/src/haystack_integrations/components/generators/llama_cpp/generator.py
+++ integrations/llama_cpp/src/haystack_integrations/components/generators/llama_cpp/generator.py
@@ -11,13 +11,17 @@ logger = logging.getLogger(__name__)
 @component
 class LlamaCppGenerator:
     """
-    Generator for using a model with Llama.cpp.
-    This component provides an interface to generate text using a quantized model (GGUF) using llama.cpp.
+    Provides an interface to generate text using LLM via llama.cpp.
+
+    [llama.cpp](https://github.com/ggerganov/llama.cpp) is a project written in C/C++ for efficient inference of LLMs.
+    It employs the quantized GGUF format, suitable for running these models on standard machines (even without GPUs).
 
     Usage example:
     ```python
-    from llama_cpp_haystack import LlamaCppGenerator
-    generator = LlamaCppGenerator(model="zephyr-7b-beta.Q4_0.gguf", n_ctx=2048, n_batch=512)
+    from haystack_integrations.components.generators.llama_cpp import LlamaCppGenerator
+    generator = LlamaCppGenerator(model="zephyr-7b-beta.Q4_0.gguf",
+                                  n_ctx=2048,
+                                  n_batch=512)
 
     print(generator.run("Who is the best American actor?", generation_kwargs={"max_tokens": 128}))
     # {'replies': ['John Cusack'], 'meta': [{"object": "text_completion", ...}]}
@@ -33,28 +37,27 @@ class LlamaCppGenerator:
         generation_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """
-        :param model: The path of a quantized model for text generation,
-            for example, "zephyr-7b-beta.Q4_0.gguf".
+        :param model: The path of a quantized model for text generation, for example, "zephyr-7b-beta.Q4_0.gguf".
             If the model path is also specified in the `model_kwargs`, this parameter will be ignored.
         :param n_ctx: The number of tokens in the context. When set to 0, the context will be taken from the model.
-            If the n_ctx is also specified in the `model_kwargs`, this parameter will be ignored.
-        :param n_batch: Prompt processing maximum batch size. Defaults to 512.
-            If the n_batch is also specified in the `model_kwargs`, this parameter will be ignored.
+            If `n_ctx` is also specified in the `model_kwargs`, this parameter will be ignored.
+        :param n_batch: Prompt processing maximum batch size.
+            If `n_batch` is also specified in the `model_kwargs`, this parameter will be ignored.
         :param model_kwargs: Dictionary containing keyword arguments used to initialize the LLM for text generation.
             These keyword arguments provide fine-grained control over the model loading.
             In case of duplication, these kwargs override `model`, `n_ctx`, and `n_batch` init parameters.
-            See Llama.cpp's [documentation](https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.__init__)
-            for more information on the available kwargs.
+            For more information on the available kwargs, see
+            [llama.cpp documentation](https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.__init__).
         :param generation_kwargs:  A dictionary containing keyword arguments to customize text generation.
             Some examples: `max_tokens`, `temperature`, `top_k`, `top_p`,...
-            See Llama.cpp's  documentation for more information:
-                https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.create_completion
+            For more information on the available kwargs, see
+            [llama.cpp documentation](https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.create_completion).
         """
 
         model_kwargs = model_kwargs or {}
         generation_kwargs = generation_kwargs or {}
 
-        # check if the huggingface_pipeline_kwargs contain the essential parameters
+        # check if the model_kwargs contain the essential parameters
         # otherwise, populate them with values from init parameters
         model_kwargs.setdefault("model_path", model)
         model_kwargs.setdefault("n_ctx", n_ctx)
@@ -76,12 +79,14 @@ class LlamaCppGenerator:
         """
         Run the text generation model on the given prompt.
 
-        :param prompt: A string representing the prompt.
-        :param generation_kwargs: A dictionary containing keyword arguments to customize text generation.
+        :param prompt: the prompt to be sent to the generative model.
+        :param generation_kwargs:  A dictionary containing keyword arguments to customize text generation.
             Some examples: `max_tokens`, `temperature`, `top_k`, `top_p`,...
-            See Llama.cpp's  documentation for more information:
-                https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.create_completion
-        :return: A dictionary of the returned responses and metadata.
+            For more information on the available kwargs, see
+            [llama.cpp documentation](https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama.create_completion).
+        :returns: A dictionary with the following keys:
+            - "replies": the list of replies generated by the model.
+            - "meta": metadata about the request.
         """
         if self.model is None:
             error_msg = "The model has not been loaded. Please call warm_up() before running."
