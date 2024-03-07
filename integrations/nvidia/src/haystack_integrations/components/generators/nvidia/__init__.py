5524e054272bc1b563b50c63a21b9b843dd707df
Silvano Cerza <silvanocerza@gmail.com>
2024-03-07 17:31:51 +0100

    Export NvidiaGeneratorModel in nvidia package

--- integrations/nvidia/src/haystack_integrations/components/generators/nvidia/__init__.py
+++ integrations/nvidia/src/haystack_integrations/components/generators/nvidia/__init__.py
@@ -2,5 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from .generator import NvidiaGenerator
+from .models import NvidiaGeneratorModel
 
-__all__ = ["NvidiaGenerator"]
+__all__ = ["NvidiaGenerator", "NvidiaGeneratorModel"]
