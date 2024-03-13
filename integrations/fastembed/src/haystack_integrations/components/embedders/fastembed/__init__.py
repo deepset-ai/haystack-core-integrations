62d84780b447f2703a9c38ec8d4b171f13b09eaa
Corentin Meyer <corentin.meyer@al-enterprise.com>
2024-03-13 16:54:29 +0100

    feat(FastEmbed): __all__ fix

--- integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/__init__.py
+++ integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/__init__.py
@@ -4,5 +4,6 @@
 from .fastembed_document_embedder import FastembedDocumentEmbedder
 from .fastembed_text_embedder import FastembedTextEmbedder
 from .fastembed_document_SPLADE_embedder import FastembedDocumentSPLADEEmbedder
+from .fastembed_text_SPLADE_embedder import FastembedTextSPLADEEmbedder
 
-__all__ = ["FastembedDocumentEmbedder", "FastembedTextEmbedder", "FastembedDocumentSPLADEEmbedder"]
+__all__ = ["FastembedDocumentEmbedder", "FastembedTextEmbedder", "FastembedDocumentSPLADEEmbedder", "FastembedTextSPLADEEmbedder"]
