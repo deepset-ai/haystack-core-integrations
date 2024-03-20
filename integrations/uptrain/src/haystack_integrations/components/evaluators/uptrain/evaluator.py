0ab16a2622f4f0214d18f77dd6729e01b9342b9e
arthurbabin <arthur.babin@gmail.com>
2024-03-20 15:03:44 +0100

    Add model specification for Azure API in Uptrain evaluator

--- integrations/uptrain/src/haystack_integrations/components/evaluators/uptrain/evaluator.py
+++ integrations/uptrain/src/haystack_integrations/components/evaluators/uptrain/evaluator.py
@@ -237,7 +237,7 @@ class UpTrainEvaluator:
                 )
                 raise ValueError(msg)
             settings = Settings(
-                model=model,
+                model=self.client_params["model"],
                 azure_api_key=api_key,
                 azure_api_version=self.client_params["azure_api_version"],
                 azure_api_base=self.client_params["azure_api_base"],
