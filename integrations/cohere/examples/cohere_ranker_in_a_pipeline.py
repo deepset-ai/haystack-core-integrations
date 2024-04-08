40fe61a4c70b9d36b419268873decb97c1336837
Anushree Bannadabhavi <anushree.bannadabhavi@gmail.com>
2024-04-08 09:27:10 -0400

    feat: Add Cohere ranker (#643)
    * Add Cohere ranker
    
    * fix linter error
    
    * Apply suggestions from code review
    
    Address review comments
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * address review comments: added a constant val, top_k in cohere client, modified tests
    
    * add ranker to pydoc conf
    
    ---------
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>

---
+++ integrations/cohere/examples/cohere_ranker_in_a_pipeline.py
@@ -0,0 +1,29 @@
+from haystack import Document, Pipeline
+from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack_integrations.components.rankers.cohere import CohereRanker
+
+# Note set your API key by running the below command in your terminal
+# export CO_API_KEY="<your Cohere API key>"
+
+docs = [
+    Document(content="Paris is in France"),
+    Document(content="Berlin is in Germany"),
+    Document(content="Lyon is in France"),
+]
+document_store = InMemoryDocumentStore()
+document_store.write_documents(docs)
+
+retriever = InMemoryBM25Retriever(document_store=document_store)
+ranker = CohereRanker(model="rerank-english-v2.0", top_k=3)
+
+document_ranker_pipeline = Pipeline()
+document_ranker_pipeline.add_component(instance=retriever, name="retriever")
+document_ranker_pipeline.add_component(instance=ranker, name="ranker")
+
+document_ranker_pipeline.connect("retriever.documents", "ranker.documents")
+
+query = "Cities in France"
+res = document_ranker_pipeline.run(
+    data={"retriever": {"query": query, "top_k": 3}, "ranker": {"query": query, "top_k": 3}}
+)
