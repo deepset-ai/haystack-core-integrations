363c7b5a96867c7b4eec2d256b600878f81f1daa
Corentin <corentin.meyer@al-enterprise.com>
2024-04-10 11:34:42 +0200

    feat(FastEmbed): Support for SPLADE Sparse Embedder (#579)
    * fix(opensearch): bulk error without create key
    
    * feat(FastEmbed): Scaffold for SPLADE Sparse Embedding Support
    
    * Revert "fix(opensearch): bulk error without create key"
    
    This reverts commit afc8e791f9f5b1f3b2c1484dc6ee6ff4493faafb.
    
    * feat(FastEmbed): __all__ fix
    
    * feat(FastEmbed): fix one test
    
    * feat(FastEmbed): fix one test
    
    * feat(FastEmbed): fix a second test
    
    * feat(FastEmbed): removed old TODO (fixed)
    
    * feat(FastEmbed): fixing all test + doc
    
    * fix output typing
    
    * Fix output component
    
    * feat(FastEmbed): renaming SPLADE to Sparse because it makes more sense
    
    * feat(FastEmbed): hatch run all lint
    
    * feat(FastEmbed): modify PR for haystack 2.1.0 with proper sparse vectors
    
    * try testing with Haystack main branch
    
    * update model name
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_document_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_document_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_document_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_document_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_text_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * feat(FastEmbed): remove prefix/suffix
    
    * feat(FastEmbed): fix linting
    
    * feat(FastEmbed): suggestion for progress bar
    
    * feat(FastEmbed): return Haystack's SparseEmbedding instead of Dict
    
    * feat(FastEmbed): fix lint
    
    * feat(Fastembed): run output type from dict to haystack sparseembedding class
    
    * feat(FastEmbed): reduce default sparse batch size
    
    * Update integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_text_embedder.py
    
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>
    
    * feat(FastEmbed): fix test
    
    * updates after 2.0.1 release
    
    * small fixes; naive example
    
    ---------
    
    Co-authored-by: anakin87 <stefanofiorucci@gmail.com>

---
+++ integrations/fastembed/src/haystack_integrations/components/embedders/fastembed/fastembed_sparse_text_embedder.py
@@ -0,0 +1,117 @@
+from typing import Any, Dict, Optional
+
+from haystack import component, default_to_dict
+from haystack.dataclasses.sparse_embedding import SparseEmbedding
+
+from .embedding_backend.fastembed_backend import _FastembedSparseEmbeddingBackendFactory
+
+
+@component
+class FastembedSparseTextEmbedder:
+    """
+    FastembedSparseTextEmbedder computes string embedding using fastembed sparse models.
+
+    Usage example:
+    ```python
+    # To use this component, install the "fastembed-haystack" package.
+    # pip install fastembed-haystack
+
+    from haystack_integrations.components.embedders.fastembed import FastembedSparseTextEmbedder
+
+    text = "It clearly says online this will work on a Mac OS system. The disk comes and it does not, only Windows. Do Not order this if you have a Mac!!"
+
+    text_embedder = FastembedSparseTextEmbedder(
+        model="prithvida/Splade_PP_en_v1"
+    )
+    text_embedder.warm_up()
+
+    embedding = text_embedder.run(text)["embedding"]
+    ```
+    """  # noqa: E501
+
+    def __init__(
+        self,
+        model: str = "prithvida/Splade_PP_en_v1",
+        cache_dir: Optional[str] = None,
+        threads: Optional[int] = None,
+        batch_size: int = 32,
+        progress_bar: bool = True,
+        parallel: Optional[int] = None,
+    ):
+        """
+        Create a FastembedSparseTextEmbedder component.
+
+        :param model: Local path or name of the model in Fastembed's model hub, such as `prithvida/Splade_PP_en_v1`
+        :param cache_dir: The path to the cache directory.
+                Can be set using the `FASTEMBED_CACHE_PATH` env variable.
+                Defaults to `fastembed_cache` in the system's temp directory.
+        :param threads: The number of threads single onnxruntime session can use. Defaults to None.
+        :param batch_size: Number of strings to encode at once.
+        :param progress_bar: If true, displays progress bar during embedding.
+        :param parallel:
+                If > 1, data-parallel encoding will be used, recommended for offline encoding of large datasets.
+                If 0, use all available cores.
+                If None, don't use data-parallel processing, use default onnxruntime threading instead.
+        """
+
+        self.model_name = model
+        self.cache_dir = cache_dir
+        self.threads = threads
+        self.batch_size = batch_size
+        self.progress_bar = progress_bar
+        self.parallel = parallel
+
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        Serializes the component to a dictionary.
+
+        :returns:
+            Dictionary with serialized data.
+        """
+        return default_to_dict(
+            self,
+            model=self.model_name,
+            cache_dir=self.cache_dir,
+            threads=self.threads,
+            batch_size=self.batch_size,
+            progress_bar=self.progress_bar,
+            parallel=self.parallel,
+        )
+
+    def warm_up(self):
+        """
+        Initializes the component.
+        """
+        if not hasattr(self, "embedding_backend"):
+            self.embedding_backend = _FastembedSparseEmbeddingBackendFactory.get_embedding_backend(
+                model_name=self.model_name, cache_dir=self.cache_dir, threads=self.threads
+            )
+
+    @component.output_types(sparse_embedding=SparseEmbedding)
+    def run(self, text: str):
+        """
+        Embeds text using the Fastembed model.
+
+        :param text: A string to embed.
+        :returns: A dictionary with the following keys:
+            - `embedding`: A list of floats representing the embedding of the input text.
+        :raises TypeError: If the input is not a string.
+        :raises RuntimeError: If the embedding model has not been loaded.
+        """
+        if not isinstance(text, str):
+            msg = (
+                "FastembedSparseTextEmbedder expects a string as input. "
+                "In case you want to embed a list of Documents, please use the FastembedDocumentEmbedder."
+            )
+            raise TypeError(msg)
+        if not hasattr(self, "embedding_backend"):
+            msg = "The embedding model has not been loaded. Please call warm_up() before running."
+            raise RuntimeError(msg)
+
+        embedding = self.embedding_backend.embed(
+            [text],
+            batch_size=self.batch_size,
+            show_progress_bar=self.progress_bar,
+            parallel=self.parallel,
+        )[0]
+        return {"sparse_embedding": embedding}
