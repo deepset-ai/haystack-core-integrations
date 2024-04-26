f3ba516824354c3ab4cad3862e3fc6866baa40da
jlonge4 <josh.longenecker1@gmail.com>
2024-04-26 19:43:35 -0400

    keyword with filters test

--- integrations/pgvector/tests/test_keyword_retrieval.py
+++ integrations/pgvector/tests/test_keyword_retrieval.py
@@ -46,10 +46,15 @@ class TestKeywordRetrieval:
 
     @pytest.mark.parametrize("document_store", ["document_store_keyword"], indirect=True)
     def test_keyword_retrieval_with_filters(self, document_store: PgvectorDocumentStore):
-        docs = [Document(content=f"Document {i} testing", embedding=rand(768).tolist()) for i in range(10)]
-
-        for i in range(10):
-            docs[i].meta["meta_field"] = "custom_value" if i % 2 == 0 else "other_value"
+        docs = [
+            Document(
+                content="The quick brown fox chased the dog",
+                embedding=([0.1] * 768),
+                meta={"meta_field": "custom_value"},
+            ),
+            Document(content="The fox was brown", embedding=([0.1] * 768), meta={"meta_field": "custom_value"}),
+            Document(content="The lazy dog", embedding=([0.1] * 768), meta={"meta_field": "custom_value"}),
+        ]
 
         document_store.write_documents(docs)
 
