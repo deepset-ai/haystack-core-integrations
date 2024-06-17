95018c909faff07504b6d7d769d4b4ee95ade56d
Vladimir Blagojevic <dovlex@gmail.com>
2024-06-17 13:16:20 +0200

    PR review

--- integrations/anthropic/src/haystack_integrations/components/generators/anthropic/chat/chat_generator.py
+++ integrations/anthropic/src/haystack_integrations/components/generators/anthropic/chat/chat_generator.py
@@ -100,8 +100,11 @@ class AnthropicChatGenerator:
             - `temperature`: The temperature to use for sampling.
             - `top_p`: The top_p value to use for nucleus sampling.
             - `top_k`: The top_k value to use for top-k sampling.
-        :param ignore_tools_thinking_messages: If True, the generator will drop so-called thinking messages when tool
-        use is detected. See the Anthropic [tools](https://docs.anthropic.com/en/docs/tool-use) for more details.
+        :param ignore_tools_thinking_messages: Anthropic's approach to tools (function calling) resolution involves a
+        "chain of thought" messages before returning the actual function names and parameters in a message. If
+        ignore_tools_thinking_messages is True, the generator will drop so-called thinking messages when tool
+        use is detected. See the Anthropic [tools](https://docs.anthropic.com/en/docs/tool-use#chain-of-thought-tool-use)
+        for more details.
         """
         self.api_key = api_key
         self.model = model
