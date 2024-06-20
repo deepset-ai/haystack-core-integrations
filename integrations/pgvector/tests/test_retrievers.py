34cae3caf38a9173e2311a70072c1de23edb34ba
Vladimir Blagojevic <dovlex@gmail.com>
2024-06-20 10:25:21 +0200

    Add filter tests

--- integrations/pgvector/tests/test_retrievers.py
+++ integrations/pgvector/tests/test_retrievers.py
@@ -135,6 +135,15 @@ class TestKeywordRetriever:
         assert retriever.filters == {"field": "value"}
         assert retriever.top_k == 5
 
+    def test_init_with_filter_policy(self, mock_store):
+        retriever = PgvectorKeywordRetriever(
+            document_store=mock_store, filters={"field": "value"}, top_k=5, filter_policy="merge"
+        )
+        assert retriever.document_store == mock_store
+        assert retriever.filters == {"field": "value"}
+        assert retriever.top_k == 5
+        assert retriever.filter_policy == "merge"
+
     def test_to_dict(self, mock_store):
         retriever = PgvectorKeywordRetriever(document_store=mock_store, filters={"field": "value"}, top_k=5)
         res = retriever.to_dict()
@@ -216,5 +225,21 @@ class TestKeywordRetriever:
         res = retriever.run(query="test query")
 
         mock_store._keyword_retrieval.assert_called_once_with(query="test query", filters={}, top_k=10)
+
+        assert res == {"documents": [doc]}
+
+    def test_run_with_filters(self):
+        mock_store = Mock(spec=PgvectorDocumentStore)
+        doc = Document(content="Test doc", embedding=[0.1, 0.2])
+        mock_store._keyword_retrieval.return_value = [doc]
+
+        retriever = PgvectorKeywordRetriever(
+            document_store=mock_store, filter_policy="merge", filters={"field": "value"}
+        )
+        res = retriever.run(query="test query", filters={"field2": "value2"})
+
+        mock_store._keyword_retrieval.assert_called_once_with(
+            query="test query", filters={"field": "value", "field2": "value2"}, top_k=10
+        )
 
         assert res == {"documents": [doc]}
