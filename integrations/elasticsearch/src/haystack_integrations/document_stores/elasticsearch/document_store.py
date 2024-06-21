bd724d42d37e232ce39a034084a0eb8158ebad64
nh-lh <nils.hilgers@lhind.dlh.de>
2024-06-21 10:27:53 +0200

    api_key & id should be a secret for elasticsearch

--- integrations/elasticsearch/src/haystack_integrations/document_stores/elasticsearch/document_store.py
+++ integrations/elasticsearch/src/haystack_integrations/document_stores/elasticsearch/document_store.py
@@ -12,6 +12,7 @@ from haystack import default_from_dict, default_to_dict
 from haystack.dataclasses import Document
 from haystack.document_stores.errors import DocumentStoreError, DuplicateDocumentError
 from haystack.document_stores.types import DuplicatePolicy
+from haystack.utils import Secret, deserialize_secrets_inplace
 from haystack.version import __version__ as haystack_version
 
 from elasticsearch import Elasticsearch, helpers  # type: ignore[import-not-found]
@@ -64,6 +65,12 @@ class ElasticsearchDocumentStore:
         hosts: Optional[Hosts] = None,
         custom_mapping: Optional[Dict[str, Any]] = None,
         index: str = "default",
+        api_key: Secret = Secret.from_env_var(
+            "DOCUMENT_STORE_PARAMS_API_KEY", strict=True
+        ),
+        api_key_id: Secret = Secret.from_env_var(
+            "DOCUMENT_STORE_PARAMS_API_KEY_ID", strict=True
+        ),
         embedding_similarity_function: Literal["cosine", "dot_product", "l2_norm", "max_inner_product"] = "cosine",
         **kwargs,
     ):
@@ -96,6 +103,8 @@ class ElasticsearchDocumentStore:
         self._index = index
         self._embedding_similarity_function = embedding_similarity_function
         self._custom_mapping = custom_mapping
+        self.api_key = api_key
+        self.api_key_id = api_key_id
         self._kwargs = kwargs
 
         if self._custom_mapping and not isinstance(self._custom_mapping, Dict):
@@ -107,6 +116,7 @@ class ElasticsearchDocumentStore:
         if self._client is None:
             client = Elasticsearch(
                 self._hosts,
+                api_key=(self.api_key_id.resolve_value(), self.api_key.resolve_value()),
                 headers={"user-agent": f"haystack-py-ds/{haystack_version}"},
                 **self._kwargs,
             )
@@ -141,6 +151,7 @@ class ElasticsearchDocumentStore:
 
             # Create the index if it doesn't exist
             if not client.indices.exists(index=self._index):
+                logger.info(f"Creating index '{self._index}' in Elasticsearch with mapping: {mappings}")
                 client.indices.create(index=self._index, mappings=mappings)
 
             self._client = client
@@ -159,6 +170,8 @@ class ElasticsearchDocumentStore:
         # type explicitly to handle this properly.
         return default_to_dict(
             self,
+            api_key=self.api_key.to_dict() if self.api_key else None,
+            api_key_id=self.api_key_id.to_dict() if self.api_key_id else None,
             hosts=self._hosts,
             custom_mapping=self._custom_mapping,
             index=self._index,
@@ -176,6 +189,7 @@ class ElasticsearchDocumentStore:
         :returns:
             Deserialized component.
         """
+        deserialize_secrets_inplace(data, keys=["api_key", "api_key_id"])
         return default_from_dict(cls, data)
 
     def count_documents(self) -> int:
