32d1cb94e99f90d9232f39c683158b353a64d00a
Amna Mubashar <amna.mubashar@Amnas-MBP.fritz.box>
2024-07-02 01:36:42 +0200

    Fixing Lint

--- integrations/chroma/tests/test_document_store.py
+++ integrations/chroma/tests/test_document_store.py
@@ -66,7 +66,7 @@ class TestDocumentStore(CountDocumentsTest, DeleteDocumentsTest, LegacyFilterDoc
             assert doc_received.meta == doc_expected.meta
 
     def test_document_store_search_without_metadata(self, document_store: ChromaDocumentStore):
-        document_store.write_documents([Document(content=e) for e in ["First document", "Second document"]] )
+        document_store.write_documents([Document(content=e) for e in ["First document", "Second document"]])
         result = document_store.search(["First document"], top_k=1)
 
         # Assertions to verify correctness
