d381b6f17b0295a8a2757cce2e5912c501fbddb4
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-03 16:35:43 +0200

    Add headers

--- integrations/weaviate/src/haystack_integrations/components/retrievers/weaviate/__init__.py
+++ integrations/weaviate/src/haystack_integrations/components/retrievers/weaviate/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from .bm25_retriever import WeaviateBM25Retriever
 from .embedding_retriever import WeaviateEmbeddingRetriever
 
