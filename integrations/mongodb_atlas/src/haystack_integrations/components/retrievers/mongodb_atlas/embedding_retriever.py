101ac3e6736f0a2c2eb7dbec3732bcbd9d20621d
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-05 11:30:10 +0200

    More leftover fixes

--- integrations/mongodb_atlas/src/haystack_integrations/components/retrievers/mongodb_atlas/embedding_retriever.py
+++ integrations/mongodb_atlas/src/haystack_integrations/components/retrievers/mongodb_atlas/embedding_retriever.py
@@ -81,7 +81,7 @@ class MongoDBAtlasEmbeddingRetriever:
             self,
             filters=self.filters,
             top_k=self.top_k,
-            filter_policy=self.filter_policy.value if self.filter_policy else None,
+            filter_policy=self.filter_policy.value,
             document_store=self.document_store.to_dict(),
         )
 
