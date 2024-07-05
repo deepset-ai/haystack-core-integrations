dc2719f183213218179bc11375cb2cccef4247d9
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-05 11:20:43 +0200

    Update integrations/pgvector/src/haystack_integrations/components/retrievers/pgvector/keyword_retriever.py
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>

--- integrations/pgvector/src/haystack_integrations/components/retrievers/pgvector/keyword_retriever.py
+++ integrations/pgvector/src/haystack_integrations/components/retrievers/pgvector/keyword_retriever.py
@@ -83,7 +83,7 @@ class PgvectorKeywordRetriever:
             self,
             filters=self.filters,
             top_k=self.top_k,
-            filter_policy=self.filter_policy.value if self.filter_policy else None,
+            filter_policy=self.filter_policy.value,
             document_store=self.document_store.to_dict(),
         )
 
