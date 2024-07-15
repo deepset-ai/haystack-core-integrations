a7f7e377cc372794dc5ed167d990ab0c106d4dfa
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-15 17:31:43 +0200

    Add unit test

--- integrations/elasticsearch/tests/test_embedding_retriever.py
+++ integrations/elasticsearch/tests/test_embedding_retriever.py
@@ -74,6 +74,29 @@ def test_from_dict(_mock_elasticsearch_client):
     assert retriever._num_candidates is None
 
 
+@patch("haystack_integrations.document_stores.elasticsearch.document_store.Elasticsearch")
+def test_from_dict_no_filter_policy(_mock_elasticsearch_client):
+    t = "haystack_integrations.components.retrievers.elasticsearch.embedding_retriever.ElasticsearchEmbeddingRetriever"
+    data = {
+        "type": t,
+        "init_parameters": {
+            "document_store": {
+                "init_parameters": {"hosts": "some fake host", "index": "default"},
+                "type": "haystack_integrations.document_stores.elasticsearch.document_store.ElasticsearchDocumentStore",
+            },
+            "filters": {},
+            "top_k": 10,
+            "num_candidates": None,
+        },
+    }
+    retriever = ElasticsearchEmbeddingRetriever.from_dict(data)
+    assert retriever._document_store
+    assert retriever._filters == {}
+    assert retriever._top_k == 10
+    assert retriever._num_candidates is None
+    assert retriever._filter_policy == FilterPolicy.REPLACE  # defaults to REPLACE
+    
+
 def test_run():
     mock_store = Mock(spec=ElasticsearchDocumentStore)
     mock_store._embedding_retrieval.return_value = [Document(content="Test doc", embedding=[0.1, 0.2])]
