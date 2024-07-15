6f80746750a68a5d88caf185f5749284ea78fcfe
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-15 17:05:52 +0200

    Add defensive check for filter_policy deserialization

--- integrations/astra/src/haystack_integrations/components/retrievers/astra/retriever.py
+++ integrations/astra/src/haystack_integrations/components/retrievers/astra/retriever.py
@@ -101,5 +101,6 @@ class AstraEmbeddingRetriever:
         """
         document_store = AstraDocumentStore.from_dict(data["init_parameters"]["document_store"])
         data["init_parameters"]["document_store"] = document_store
-        data["init_parameters"]["filter_policy"] = FilterPolicy.from_str(data["init_parameters"]["filter_policy"])
+        if filter_policy := data["init_parameters"].get("filter_policy"):
+            data["init_parameters"]["filter_policy"] = FilterPolicy.from_str(filter_policy)
         return default_from_dict(cls, data)
