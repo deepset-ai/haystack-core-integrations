db2b5f72bd1c724eb346a3e4b55c3b416a1fd821
Vladimir Blagojevic <dovlex@gmail.com>
2024-07-17 11:04:57 +0200

    Add defensive check for filter_policy deserialization (#903)

--- integrations/weaviate/tests/test_embedding_retriever.py
+++ integrations/weaviate/tests/test_embedding_retriever.py
@@ -114,6 +114,49 @@ def test_from_dict(_mock_weaviate):
     assert retriever._certainty is None
 
 
+@patch("haystack_integrations.document_stores.weaviate.document_store.weaviate")
+def test_from_dict_no_filter_policy(_mock_weaviate):
+    retriever = WeaviateEmbeddingRetriever.from_dict(
+        {
+            "type": "haystack_integrations.components.retrievers.weaviate.embedding_retriever.WeaviateEmbeddingRetriever",  # noqa: E501
+            "init_parameters": {
+                "filters": {},
+                "top_k": 10,
+                "distance": None,
+                "certainty": None,
+                "document_store": {
+                    "type": "haystack_integrations.document_stores.weaviate.document_store.WeaviateDocumentStore",
+                    "init_parameters": {
+                        "url": None,
+                        "collection_settings": {
+                            "class": "Default",
+                            "invertedIndexConfig": {"indexNullState": True},
+                            "properties": [
+                                {"name": "_original_id", "dataType": ["text"]},
+                                {"name": "content", "dataType": ["text"]},
+                                {"name": "dataframe", "dataType": ["text"]},
+                                {"name": "blob_data", "dataType": ["blob"]},
+                                {"name": "blob_mime_type", "dataType": ["text"]},
+                                {"name": "score", "dataType": ["number"]},
+                            ],
+                        },
+                        "auth_client_secret": None,
+                        "additional_headers": None,
+                        "embedded_options": None,
+                        "additional_config": None,
+                    },
+                },
+            },
+        }
+    )
+    assert retriever._document_store
+    assert retriever._filters == {}
+    assert retriever._top_k == 10
+    assert retriever._distance is None
+    assert retriever._certainty is None
+    assert retriever._filter_policy == FilterPolicy.REPLACE  # defaults to REPLACE
+
+
 @patch("haystack_integrations.components.retrievers.weaviate.bm25_retriever.WeaviateDocumentStore")
 def test_run(mock_document_store):
     retriever = WeaviateEmbeddingRetriever(document_store=mock_document_store)
