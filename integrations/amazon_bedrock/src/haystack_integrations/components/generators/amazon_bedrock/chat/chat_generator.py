3f1239c7315e911f0bbaf93d442576caec5c39c1
David S. Batista <dsbatista@gmail.com>
2024-07-17 16:56:49 +0200

    fixing 3.9 linting issues

--- integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/chat/chat_generator.py
+++ integrations/amazon_bedrock/src/haystack_integrations/components/generators/amazon_bedrock/chat/chat_generator.py
@@ -138,7 +138,8 @@ class AmazonBedrockChatGenerator:
 
         warnings.warn(
             "The `meta` output of the AmazonBedrockChatGenerator will change in the next release to be inline with "
-            "OpenAI `meta`output keys.", stacklevel=2
+            "OpenAI `meta`output keys.",
+            stacklevel=2
         )
 
     def invoke(self, *args, **kwargs):
--- integrations/anthropic/src/haystack_integrations/components/generators/anthropic/chat/chat_generator.py
+++ integrations/anthropic/src/haystack_integrations/components/generators/anthropic/chat/chat_generator.py
@@ -117,7 +117,8 @@ class AnthropicChatGenerator:
 
         warnings.warn(
             "The `meta` output of the AnthropicChatGenerator will change in the next release to be inline with "
-            "OpenAI `meta`output keys.", stacklevel=2
+            "OpenAI `meta`output keys.",
+            stacklevel=2
         )
 
     def _get_telemetry_data(self) -> Dict[str, Any]:
