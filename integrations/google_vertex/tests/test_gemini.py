f4831e7a66453145162140d65ba8bbdfb98090e3
Amna Mubashar <amnahkhan.ak@gmail.com>
2024-08-21 10:51:50 +0200

    Fix mock in test file

--- integrations/google_vertex/tests/test_gemini.py
+++ integrations/google_vertex/tests/test_gemini.py
@@ -3,7 +3,6 @@ from unittest.mock import MagicMock, Mock, patch
 from vertexai.preview.generative_models import (
     FunctionDeclaration,
     GenerationConfig,
-    GenerativeModel,
     HarmBlockThreshold,
     HarmCategory,
     Tool,
@@ -30,8 +29,9 @@ GET_CURRENT_WEATHER_FUNC = FunctionDeclaration(
     },
 )
 
-
-def test_init():
+@patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai.init")
+@patch("haystack_integrations.components.generators.google_vertex.gemini.GenerativeModel")
+def test_init(_mock_vertexai_init, mock_generative_model):
 
     generation_config = GenerationConfig(
         candidate_count=1,
@@ -44,25 +44,24 @@ def test_init():
     safety_settings = {HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH}
 
     tool = Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])
-    with patch(
-        "haystack_integrations.components.generators.google_vertex.gemini.vertexai.init"
-    ) as mock_genai_configure:
-        gemini = VertexAIGeminiGenerator(
-            project_id="TestID123",
-            location="TestLocation",
-            generation_config=generation_config,
-            safety_settings=safety_settings,
-            tools=[tool],
-        )
-    mock_genai_configure.assert_called()
+
+    gemini = VertexAIGeminiGenerator(
+        project_id="TestID123",
+        location="TestLocation",
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        tools=[tool],
+    )
+    _mock_vertexai_init.assert_called()
     assert gemini._model_name == "gemini-pro-vision"
     assert gemini._generation_config == generation_config
     assert gemini._safety_settings == safety_settings
     assert gemini._tools == [tool]
-    assert isinstance(gemini._model, GenerativeModel)
 
+@patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai")
+@patch("haystack_integrations.components.generators.google_vertex.gemini.GenerativeModel")
 
-def test_to_dict():
+def test_to_dict(_mock_vertexai, _mock_generative_model):
     generation_config = GenerationConfig(
         candidate_count=1,
         stop_sequences=["stop"],
@@ -75,13 +74,12 @@ def test_to_dict():
 
     tool = Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])
 
-    with patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai.init"):
-        gemini = VertexAIGeminiGenerator(
-            project_id="TestID123",
-            generation_config=generation_config,
-            safety_settings=safety_settings,
-            tools=[tool],
-        )
+    gemini = VertexAIGeminiGenerator(
+        project_id="TestID123",
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        tools=[tool],
+    )
     assert gemini.to_dict() == {
         "type": "haystack_integrations.components.generators.google_vertex.gemini.VertexAIGeminiGenerator",
         "init_parameters": {
@@ -123,51 +121,52 @@ def test_to_dict():
     }
 
 
-def test_from_dict():
+@patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai")
+@patch("haystack_integrations.components.generators.google_vertex.gemini.GenerativeModel")
 
-    with patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai.init"):
-        gemini = VertexAIGeminiGenerator.from_dict(
-            {
-                "type": "haystack_integrations.components.generators.google_vertex.gemini.VertexAIGeminiGenerator",
-                "init_parameters": {
-                    "project_id": "TestID123",
-                    "model": "gemini-pro-vision",
-                    "generation_config": {
-                        "temperature": 0.5,
-                        "top_p": 0.5,
-                        "top_k": 0.5,
-                        "candidate_count": 1,
-                        "max_output_tokens": 10,
-                        "stop_sequences": ["stop"],
-                    },
-                    "safety_settings": {
-                        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH
-                    },
-                    "tools": [
-                        {
-                            "function_declarations": [
-                                {
-                                    "name": "get_current_weather",
-                                    "description": "Get the current weather in a given location",
-                                    "parameters": {
-                                        "type_": "OBJECT",
-                                        "properties": {
-                                            "location": {
-                                                "type_": "STRING",
-                                                "description": "The city and state, e.g. San Francisco, CA",
-                                            },
-                                            "unit": {"type_": "STRING", "enum": ["celsius", "fahrenheit"]},
-                                        },
-                                        "required": ["location"],
-                                    },
-                                }
-                            ]
-                        }
-                    ],
-                    "streaming_callback": None,
+def test_from_dict(_mock_vertexai, _mock_generative_model):
+    gemini = VertexAIGeminiGenerator.from_dict(
+        {
+            "type": "haystack_integrations.components.generators.google_vertex.gemini.VertexAIGeminiGenerator",
+            "init_parameters": {
+                "project_id": "TestID123",
+                "model": "gemini-pro-vision",
+                "generation_config": {
+                    "temperature": 0.5,
+                    "top_p": 0.5,
+                    "top_k": 0.5,
+                    "candidate_count": 1,
+                    "max_output_tokens": 10,
+                    "stop_sequences": ["stop"],
                 },
-            }
-        )
+                "safety_settings": {
+                    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH
+                },
+                "tools": [
+                    {
+                        "function_declarations": [
+                            {
+                                "name": "get_current_weather",
+                                "description": "Get the current weather in a given location",
+                                "parameters": {
+                                    "type_": "OBJECT",
+                                    "properties": {
+                                        "location": {
+                                            "type_": "STRING",
+                                            "description": "The city and state, e.g. San Francisco, CA",
+                                        },
+                                        "unit": {"type_": "STRING", "enum": ["celsius", "fahrenheit"]},
+                                    },
+                                    "required": ["location"],
+                                },
+                            }
+                        ]
+                    }
+                ],
+                "streaming_callback": None,
+            },
+        }
+    )
 
     assert gemini._model_name == "gemini-pro-vision"
     assert gemini._project_id == "TestID123"
@@ -175,15 +174,20 @@ def test_from_dict():
 
     # assert gemini._tools == [Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])]
     assert isinstance(gemini._generation_config, GenerationConfig)
-    assert isinstance(gemini._model, GenerativeModel)
 
 
-@patch("haystack_integrations.components.generators.google_vertex.gemini.vertexai.init")
-@patch("haystack_integrations.components.generators.google_vertex.gemini.VertexAIGeminiGenerator")
-def test_run(mock_model_class, _mock_vertexai_init):
+@patch("haystack_integrations.components.generators.google_vertex.gemini.GenerativeModel")
+def test_run(mock_generative_model):
     mock_model = Mock()
-    mock_model.predict.return_value = MagicMock()
-    mock_model_class.from_pretrained.return_value = mock_model
-    VertexAIGeminiGenerator(model="gemini-pro", project_id="TestID123")
+    mock_model.generate_content.return_value = MagicMock()
+    mock_generative_model.return_value = mock_model
 
-    _mock_vertexai_init.assert_called_once_with(project="TestID123", location=None)
+    gemini = VertexAIGeminiGenerator(project_id="TestID123", location=None)
+
+    response = gemini.run(["What's the weather like today?"])
+
+    mock_model.generate_content.assert_called_once()
+    assert "replies" in response
+    assert isinstance(response["replies"], list)
+
+
