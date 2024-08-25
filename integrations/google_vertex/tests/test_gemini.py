19f7db43066998fd919f50f3e521d675f351577f
Amna Mubashar <amnahkhan.ak@gmail.com>
2024-08-26 01:00:30 +0200

    Fix assertion

--- integrations/google_vertex/tests/test_gemini.py
+++ integrations/google_vertex/tests/test_gemini.py
@@ -215,8 +215,7 @@ def test_from_dict_with_param(_mock_vertexai_init, _mock_generative_model):
     assert gemini._model_name == "gemini-pro-vision"
     assert gemini._project_id == "TestID123"
     assert gemini._safety_settings == {HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH}
-
-    # assert gemini._tools == [Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])]
+    assert repr(gemini._tools) == repr([Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])])
     assert isinstance(gemini._generation_config, GenerationConfig)
 
 
