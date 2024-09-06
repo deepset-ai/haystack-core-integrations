722af9db30449dc3ac363313955cb4e9e0229c0d
Amna Mubashar <amnahkhan.ak@gmail.com>
2024-09-06 14:17:51 +0200

    Minor fix

--- integrations/google_vertex/tests/chat/test_gemini.py
+++ integrations/google_vertex/tests/chat/test_gemini.py
@@ -232,7 +232,6 @@ def test_from_dict_with_param(_mock_vertexai_init, _mock_generative_model):
     assert gemini._safety_settings == {HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH}
     assert repr(gemini._tools) == repr([Tool(function_declarations=[GET_CURRENT_WEATHER_FUNC])])
     assert isinstance(gemini._generation_config, GenerationConfig)
-    assert isinstance(gemini._tools[0], Tool)
 
 
 @patch("haystack_integrations.components.generators.google_vertex.chat.gemini.GenerativeModel")
