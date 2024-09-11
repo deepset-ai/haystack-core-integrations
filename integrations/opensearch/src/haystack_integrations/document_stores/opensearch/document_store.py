673a937b4724a9d401ac1b7cae579a0eb7601713
Vladimir Blagojevic <dovlex@gmail.com>
2024-09-11 12:43:39 +0200

    More formatting

--- integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
+++ integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
@@ -237,7 +237,7 @@ class OpenSearchDocumentStore:
 
     def filter_documents(self, filters: Optional[Dict[str, Any]] = None) -> List[Document]:
         if filters and "operator" not in filters and "conditions" not in filters:
-            msg = "Legacy filters support has been removed. See https://docs.haystack.deepset.ai/docs/metadata-filtering for more details."
+            msg = "Invalid filter syntax. See https://docs.haystack.deepset.ai/docs/metadata-filtering for details."
             raise ValueError(msg)
 
         if filters:
