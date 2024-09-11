e0b3a96c85a2bd41d3e894c6a8195a8f4838741f
Vladimir Blagojevic <dovlex@gmail.com>
2024-09-11 12:37:13 +0200

    Error msg fmt

--- integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
+++ integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
@@ -383,7 +383,7 @@ class OpenSearchDocumentStore:
         :returns: List of Document that match `query`
         """
         if filters and "operator" not in filters and "conditions" not in filters:
-            msg = "Legacy filters support has been removed. Please see documentation for new filter syntax."
+            msg = "Invalid filter syntax. See https://docs.haystack.deepset.ai/docs/metadata-filtering for details."
             raise ValueError(msg)
 
         if not query:
