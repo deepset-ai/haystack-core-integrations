60c4d4a54997c1ee8d0834bd8785e25221be9a7c
Aaron Ji <127167174+DresAaron@users.noreply.github.com>
2024-09-18 20:50:01 +0800

    docs: update docstrings of JinaDocumentEmbedder and JinaTextEmbedder (#1092)
    * chore: update function doc
    
    * chore: polished function doc
    
    * chore: polished function doc
    
    * chore: update function doc
    
    * Update integrations/jina/src/haystack_integrations/components/embedders/jina/document_embedder.py
    
    Co-authored-by: Julian Risch <julianrisch@gmx.de>
    
    * Update integrations/jina/src/haystack_integrations/components/embedders/jina/text_embedder.py
    
    Co-authored-by: Julian Risch <julianrisch@gmx.de>
    
    * chore: update function doc
    
    * fix: lint issues
    
    * chore: update function doc
    
    ---------
    
    Co-authored-by: Julian Risch <julianrisch@gmx.de>

--- integrations/jina/src/haystack_integrations/components/embedders/jina/text_embedder.py
+++ integrations/jina/src/haystack_integrations/components/embedders/jina/text_embedder.py
@@ -52,6 +52,16 @@ class JinaTextEmbedder:
             Check the list of available models on [Jina documentation](https://jina.ai/embeddings/).
         :param prefix: A string to add to the beginning of each text.
         :param suffix: A string to add to the end of each text.
+        :param task: The downstream task for which the embeddings will be used.
+            The model will return the optimized embeddings for that task.
+            Check the list of available tasks on [Jina documentation](https://jina.ai/embeddings/).
+        :param dimensions: Number of desired dimension.
+            Smaller dimensions are easier to store and retrieve, with minimal performance impact thanks to MRL.
+        :param late_chunking: A boolean to enable or disable late chunking.
+            Apply the late chunking technique to leverage the model's long-context capabilities for
+            generating contextual chunk embeddings.
+
+            The support of `task` and `late_chunking` parameters is only available for jina-embeddings-v3.
         """
 
         resolved_api_key = api_key.resolve_value()
