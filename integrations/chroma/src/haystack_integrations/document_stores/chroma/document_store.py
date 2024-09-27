3076bfc62d83e5ade25ffbd43e20f658de38202b
alperkaya <alper.kaya@mail.com>
2024-09-27 15:44:35 +0200

    addressed comments

--- integrations/chroma/src/haystack_integrations/document_stores/chroma/document_store.py
+++ integrations/chroma/src/haystack_integrations/document_stores/chroma/document_store.py
@@ -94,59 +94,51 @@ class ChromaDocumentStore:
 
     def _ensure_initialized(self):
         if not self._initialized:
-            self._chroma_client = self.chroma_client()
-            self._collection = self.collection()
-            self._initialized = True
-
-    def chroma_client(self):
-        if self._chroma_client is None:
-            # Create the client instance
-            if self._persist_path and (self._host or self._port is not None):
-                error_message = (
-                    "You must specify `persist_path` for local persistent storage or, "
-                    "alternatively, `host` and `port` for remote HTTP client connection. "
-                    "You cannot specify both options."
-                )
-                raise ValueError(error_message)
-            if self._host and self._port is not None:
-                # Remote connection via HTTP client
-                self._chroma_client = chromadb.HttpClient(
-                    host=self._host,
-                    port=self._port,
-                )
-            elif self._persist_path is None:
-                # In-memory storage
-                self._chroma_client = chromadb.Client()
-            else:
-                # Local persistent storage
-                self._chroma_client = chromadb.PersistentClient(path=self._persist_path)
-
-        return self._chroma_client
-
-    def collection(self):
-        if self._collection is None:
-
-            self._metadata = self._metadata or {}
-            if "hnsw:space" not in self._metadata:
-                self._metadata["hnsw:space"] = self._distance_function
-
-            if self._collection_name in [c.name for c in self._chroma_client.list_collections()]:
-                self._collection = self._chroma_client.get_collection(
-                    self._collection_name, embedding_function=self._embedding_func
-                )
-
-                if self._metadata != self._collection.metadata:
-                    logger.warning(
-                        "Collection already exists. The `distance_function` and `metadata` parameters will be ignored."
+            if self._chroma_client is None:
+                # Create the client instance
+                if self._persist_path and (self._host or self._port is not None):
+                    error_message = (
+                        "You must specify `persist_path` for local persistent storage or, "
+                        "alternatively, `host` and `port` for remote HTTP client connection. "
+                        "You cannot specify both options."
                     )
-            else:
-                self._collection = self._chroma_client.create_collection(
-                    name=self._collection_name,
-                    metadata=self._metadata,
-                    embedding_function=self._embedding_func,
-                )
+                    raise ValueError(error_message)
+                if self._host and self._port is not None:
+                    # Remote connection via HTTP client
+                    self._chroma_client = chromadb.HttpClient(
+                        host=self._host,
+                        port=self._port,
+                    )
+                elif self._persist_path is None:
+                    # In-memory storage
+                    self._chroma_client = chromadb.Client()
+                else:
+                    # Local persistent storage
+                    self._chroma_client = chromadb.PersistentClient(path=self._persist_path)
 
-        return self._collection
+            if self._collection is None:
+                self._metadata = self._metadata or {}
+                if "hnsw:space" not in self._metadata:
+                    self._metadata["hnsw:space"] = self._distance_function
+
+                if self._collection_name in [c.name for c in self._chroma_client.list_collections()]:
+                    self._collection = self._chroma_client.get_collection(
+                        self._collection_name, embedding_function=self._embedding_func
+                    )
+
+                    if self._metadata != self._collection.metadata:
+                        logger.warning(
+                            "Collection already exists. "
+                            "The `distance_function` and `metadata` parameters will be ignored."
+                        )
+                else:
+                    self._collection = self._chroma_client.create_collection(
+                        name=self._collection_name,
+                        metadata=self._metadata,
+                        embedding_function=self._embedding_func,
+                    )
+
+            self._initialized = True
 
     def count_documents(self) -> int:
         """
@@ -155,9 +147,7 @@ class ChromaDocumentStore:
         :returns: how many documents are present in the document store.
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
         return self._collection.count()
 
     def filter_documents(self, filters: Optional[Dict[str, Any]] = None) -> List[Document]:
@@ -223,9 +213,7 @@ class ChromaDocumentStore:
          :returns: a list of Documents that match the given filters.
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
 
         if filters:
             chroma_filter = _convert_filters(filters)
@@ -258,9 +246,7 @@ class ChromaDocumentStore:
             The number of documents written
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
 
         for doc in documents:
             if not isinstance(doc, Document):
@@ -318,9 +304,7 @@ class ChromaDocumentStore:
         :param document_ids: the object_ids to delete
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
 
         self._collection.delete(ids=document_ids)
 
@@ -333,9 +317,7 @@ class ChromaDocumentStore:
         :returns: matching documents for each query.
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
 
         if filters is None:
             results = self._collection.query(
@@ -369,9 +351,7 @@ class ChromaDocumentStore:
 
         """
         self._ensure_initialized()
-        if self._collection is None:
-            msg = "Collection is not initialized"
-            raise ValueError(msg)
+        assert self._collection is not None
 
         if filters is None:
             results = self._collection.query(
