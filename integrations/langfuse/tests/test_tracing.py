3511eb39176c40186f628721fcd6407e7bcf5f76
Vladimir Blagojevic <dovlex@gmail.com>
2024-10-01 16:45:32 +0200

    Langfuse: add invocation_context to identify traces (#1089)
    * Langfuse: add invocation_context to identify traces
    
    * Lint
    
    * Add unit test
    
    * Log debug invocation context
    
    * Lint + format

--- integrations/langfuse/tests/test_tracing.py
+++ integrations/langfuse/tests/test_tracing.py
@@ -43,7 +43,12 @@ def test_tracing_integration(llm_class, env_var, expected_trace):
         ChatMessage.from_user("Tell me about {{location}}"),
     ]
 
-    response = pipe.run(data={"prompt_builder": {"template_variables": {"location": "Berlin"}, "template": messages}})
+    response = pipe.run(
+        data={
+            "prompt_builder": {"template_variables": {"location": "Berlin"}, "template": messages},
+            "tracer": {"invocation_context": {"user_id": "user_42"}},
+        }
+    )
     assert "Berlin" in response["llm"]["replies"][0].content
     assert response["tracer"]["trace_url"]
 
@@ -65,5 +70,7 @@ def test_tracing_integration(llm_class, env_var, expected_trace):
         assert expected_trace in str(response.content)
         # check if the trace contains the expected generation span
         assert "GENERATION" in str(response.content)
+        # check if the trace contains the expected user_id
+        assert "user_42" in str(response.content)
     except requests.exceptions.RequestException as e:
         pytest.fail(f"Failed to retrieve data from Langfuse API: {e}")
