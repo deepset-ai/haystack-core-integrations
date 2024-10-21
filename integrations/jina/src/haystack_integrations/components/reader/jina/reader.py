f71d7b46379d6d7507cedb09a867e5cc22607ff7
jlonge4 <josh.longenecker1@gmail.com>
2024-10-21 18:36:02 -0400

    add pipeline functions

--- integrations/jina/src/haystack_integrations/components/reader/jina/reader.py
+++ integrations/jina/src/haystack_integrations/components/reader/jina/reader.py
@@ -2,11 +2,11 @@
 # SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+import urllib
+from enum import Enum
 from typing import Any, Dict, List, Optional, Union
 
 import requests
-import urllib
-from enum import Enum
 from haystack import Document, component, default_from_dict, default_to_dict
 from haystack.utils import Secret, deserialize_secrets_inplace
 
@@ -46,22 +46,17 @@ class JinaReaderMode(Enum):
             raise ValueError(msg)
         return reader_mode
 
-@component
-class JinaReader():
 
+@component
+class JinaReader:
     def __init__(
         self,
         mode: Union[JinaReaderMode, str],
-        url: Optional[str] = None,
-        reader_query: Optional[str] = None,
         api_key: Secret = Secret.from_env_var("JINA_API_KEY"),
     ):
-
         resolved_api_key = api_key.resolve_value()
         self.api_key = api_key
         self.mode = mode
-        self.url = url
-        self.reader_query = reader_query
 
         self._session = requests.Session()
         self._session.headers.update(
@@ -72,27 +67,39 @@ class JinaReader():
             }
         )
 
-    @component.output_types(document=Document)
-    def run(self, input:str):
-
-        # check input depending on mode
-        mode_map = {
-            JinaReaderMode.READ: "r",
-            JinaReaderMode.SEARCH: "s",
-            JinaReaderMode.GROUND: "g"
-        }
-        base_url = "https://{}.jina.ai/".format(mode_map[self.mode])
-        encoded_target = urllib.parse.quote(input, safe="")
-        url = f"{base_url}{encoded_target}"
-        response = self._session.get(
-            url
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        Serializes the component to a dictionary.
+        :returns:
+            Dictionary with serialized data.
+        """
+        return default_to_dict(
+            self,
+            api_key=self.api_key.to_dict(),
+            mode=self.mode,
         )
-        metadata = {
-            'content_type': response.headers['Content-Type'],
-            'url':input
-            }
-        document = [Document(content = response.content, meta = metadata)]
-        return document
 
-        ... # do the rest and clean ups
-        
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "JinaReader":
+        """
+        Deserializes the component from a dictionary.
+        :param data:
+            Dictionary to deserialize from.
+        :returns:
+            Deserialized component.
+        """
+        deserialize_secrets_inplace(data["init_parameters"], keys=["api_key"])
+        return default_from_dict(cls, data)
+
+    @component.output_types(document=Document)
+    def run(self, query: str):
+        # check input depending on mode
+        mode_map = {JinaReaderMode.READ: "r", JinaReaderMode.SEARCH: "s", JinaReaderMode.GROUND: "g"}
+        mode = mode_map[self.mode]
+        base_url = f"https://{mode}.jina.ai/"
+        encoded_target = urllib.parse.quote(query, safe="")
+        url = f"{base_url}{encoded_target}"
+        response = self._session.get(url)
+        metadata = {"content_type": response.headers["Content-Type"], "url": input}
+        document = [Document(content=response.content, meta=metadata)]
+        return document
