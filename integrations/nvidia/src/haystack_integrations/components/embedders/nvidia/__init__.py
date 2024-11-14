f1fd742998f2f47bec8eac9785596e358fd0b9c5
Sebastian Husch Lee <sjrl@users.noreply.github.com>
2024-11-14 14:53:54 +0100

    fix: Fixes to NvidiaRanker (#1191)
    * Fixes to NvidiaRanker
    
    * Add inits and headers
    
    * More headers
    
    * updates
    
    * Reactivate test
    
    * Fix tests
    
    * Reenable test and add test

--- integrations/nvidia/src/haystack_integrations/components/embedders/nvidia/__init__.py
+++ integrations/nvidia/src/haystack_integrations/components/embedders/nvidia/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from .document_embedder import NvidiaDocumentEmbedder
 from .text_embedder import NvidiaTextEmbedder
 from .truncate import EmbeddingTruncateMode
