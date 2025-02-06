c00b8d4b53c01e41e4c1b777c794387ed9c3a43e
David S. Batista <dsbatista@gmail.com>
2025-02-06 17:23:04 +0100

    linting

--- integrations/mistral/src/haystack_integrations/components/generators/mistral/chat/chat_generator.py
+++ integrations/mistral/src/haystack_integrations/components/generators/mistral/chat/chat_generator.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-
 from typing import Any, Callable, Dict, Optional, List
 
 from haystack import component
