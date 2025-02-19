e9f2397e58156dd0577f6b97e7b33371fe9c7f07
Stefano Fiorucci <stefanofiorucci@gmail.com>
2025-02-19 11:54:29 +0100

    chore!: Qdrant - remove dataframe support (#1403)
    * qdrant - remove dataframe support
    
    * simplify checks
    
    * simplification

--- integrations/qdrant/tests/test_converters.py
+++ integrations/qdrant/tests/test_converters.py
@@ -1,7 +1,10 @@
 import numpy as np
+from haystack import Document
+from pandas import DataFrame
 from qdrant_client.http import models as rest
 
 from haystack_integrations.document_stores.qdrant.converters import (
+    convert_haystack_documents_to_qdrant_points,
     convert_id,
     convert_qdrant_point_to_haystack_document,
 )
@@ -62,3 +65,44 @@ def test_point_to_document_reverts_proper_structure_from_record_without_sparse()
     assert document.sparse_embedding is None
     assert {"test_field": 1} == document.meta
     assert 0.0 == np.sum(np.array([1.0, 0.0, 0.0, 0.0]) - document.embedding)
+
+
+def test_point_to_document_skips_dataframe():
+
+    point = rest.Record(
+        id="c7c62e8e-02b9-4ec6-9f88-46bd97b628b7",
+        payload={
+            "id": "my-id",
+            "content": "Lorem ipsum",
+            "content_type": "text",
+            "meta": {
+                "test_field": 1,
+            },
+            "dataframe": {"a": [1, 2, 3]},
+        },
+        vector=[1.0, 0.0, 0.0, 0.0],
+    )
+    document = convert_qdrant_point_to_haystack_document(point, use_sparse_embeddings=False)
+    assert "my-id" == document.id
+    assert "Lorem ipsum" == document.content
+    assert "text" == document.content_type
+    assert {"test_field": 1} == document.meta
+    assert 0.0 == np.sum(np.array([1.0, 0.0, 0.0, 0.0]) - document.embedding)
+    assert not hasattr(document, "dataframe") or document.dataframe is None
+
+
+def test_documents_to_points_skips_dataframe():
+    doc = Document(
+        id="my-id",
+        content="Lorem ipsum",
+        embedding=[1.0, 0.0, 0.0, 0.0],
+    )
+
+    doc.dataframe = DataFrame([[1, 2], [3, 4]])
+
+    points = convert_haystack_documents_to_qdrant_points([doc], use_sparse_embeddings=False)
+    assert len(points) == 1
+
+    assert points[0].payload["content"] == "Lorem ipsum"
+    assert points[0].vector == [1.0, 0.0, 0.0, 0.0]
+    assert "dataframe" not in points[0].payload
