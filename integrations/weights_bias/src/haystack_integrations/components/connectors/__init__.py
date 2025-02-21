a4c92b679eb339c20f17f0f10d7c63b77925411d
David S. Batista <dsbatista@gmail.com>
2025-02-21 12:41:51 +0100

    feat: integration with Weave the tracer from Weight and Bias  (#1412)
    * initial import
    
    * adding LICENSE, pydoc and pyproject.toml
    
    * linting
    
    * adding main README.md
    
    * adding code and tests
    
    * updating labeler.yml
    
    * updating pyproject.toml
    
    * fixing typing
    
    * updating workflow
    
    * wip: adding env variable
    
    * marking tests as integration tests and adding API key to github workflow
    
    * linting
    
    * linting
    
    * writing README.md
    
    * adding pytest.markers to pyproject.toml
    
    * adding pytest.markers to pyproject.toml
    
    * completing doc strings
    
    * linting
    
    * linting
    
    * changing package name to weights-bias-haystack
    
    * wip: PR review
    
    * fixing pyproject.toml
    
    * fixing linting and typing
    
    * some more small fixes
    
    * improving docstring and reading HAYSTACK_CONTENT_TRACING_ENABLED value
    
    * make create_call part of WeaveTracer
    
    * adding more docstrings and raising the execption
    
    * updating tests
    
    * docstrings
    
    * removing f-string from Exception
    
    * adding shorter example to README.md
    
    * fixing the empty raise
    
    * reverting raise
    
    * explicitly making the WeaveConnector.run() method a no-op component
    
    * nit: fixing example
    
    * nit: fixing example
    
    * nit: fixing example
    
    * fixing README
    
    * refactoring trace method from WeaveTracer
    
    * linting style
    
    * sending pipeline_name to WaB and checking the env variable in the Tracer init
    
    * completely removing env check from the connector
    
    * remove unused imports
    
    * adding tracer tests
    
    * formatting + linting in tests!
    
    * fixing connector tests
    
    * monkeypatch safe delete for undeclared env vars
    
    * tests for the tracer
    
    * reverting diff message
    
    * fixing tests

---
+++ integrations/weights_bias/src/haystack_integrations/components/connectors/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .weave_connector import WeaveConnector
+
+__all__ = ["WeaveConnector"]
