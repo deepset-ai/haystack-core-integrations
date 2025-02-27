02a2f37b4bd8226992430c335aed974f0aa3e7e9
Julian Risch <julian.risch@deepset.ai>
2025-02-27 17:30:06 +0100

    feat: add STACKITChatGenerator and Embedders (#1274)
    * add STACKITChatGenerator
    
    * add model to component init in tests
    
    * Apply docstring suggestions from code review
    
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>
    
    * Update integrations/stackit/src/haystack_integrations/components/generators/stackit/chat/chat_generator.py
    
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>
    
    * address review comments
    
    * update chat generator
    
    * add text and document embedder
    
    * address review comments and linter
    
    * add workflow file
    
    * ignore print in examples
    
    * lint
    
    ---------
    
    Co-authored-by: Daria Fokina <daria.fokina@deepset.ai>

---
+++ integrations/stackit/src/haystack_integrations/components/embedders/stackit/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .document_embedder import STACKITDocumentEmbedder
+from .text_embedder import STACKITTextEmbedder
+
+__all__ = ["STACKITDocumentEmbedder", "STACKITTextEmbedder"]
