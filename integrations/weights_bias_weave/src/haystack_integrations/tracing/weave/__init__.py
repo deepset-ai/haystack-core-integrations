c300aa4501ccda519b94eebd6ef1c343ae96a92f
David S. Batista <dsbatista@gmail.com>
2025-03-27 10:58:16 +0100

    renaming to weights_and_biases_weave

--- integrations/weights_bias_weave/src/haystack_integrations/tracing/weave/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
-#
-# SPDX-License-Identifier: Apache-2.0
-
-from .tracer import WeaveSpan, WeaveTracer
-
-__all__ = ["WeaveSpan", "WeaveTracer"]
