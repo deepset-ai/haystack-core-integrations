049566dcebee07637610ac633157f4a0b2dd8a2d
Sebastian Husch Lee <sjrl423@gmail.com>
2025-04-02 08:05:50 +0200

    Fix tests

--- integrations/cohere/tests/test_cohere_chat_generator.py
+++ integrations/cohere/tests/test_cohere_chat_generator.py
@@ -462,7 +462,12 @@ class TestCohereChatGenerator:
         if not hasattr(pipeline, "_connection_type_validation"):
             expected_dict.pop("connection_type_validation")
 
-        # add inputs_from_state and outputs_to_state tool parameters for compatibility with haystack-ai>=2.12.0
+        # add outputs_to_string, inputs_from_state and outputs_to_state tool parameters for compatibility with
+        # haystack-ai>=2.12.0
+        if hasattr(tool, "outputs_to_string"):
+            expected_dict["components"]["generator"]["init_parameters"]["tools"][0]["data"][
+                "outputs_to_string"
+            ] = tool.outputs_to_string
         if hasattr(tool, "inputs_from_state"):
             expected_dict["components"]["generator"]["init_parameters"]["tools"][0]["data"][
                 "inputs_from_state"
