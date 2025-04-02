b3eb70ae7f8062ada501b2a3800989ae9fa17191
Amna Mubashar <amnahkhan.ak@gmail.com>
2025-04-02 20:07:43 +0500

    rename env variables

--- integrations/azure_ai_search/src/haystack_integrations/document_stores/azure_ai_search/document_store.py
+++ integrations/azure_ai_search/src/haystack_integrations/document_stores/azure_ai_search/document_store.py
@@ -97,12 +97,12 @@ class AzureAISearchDocumentStore:
         For more information on parameters, see the [official Azure AI Search documentation](https://learn.microsoft.com/en-us/azure/search/).
         """
 
-        azure_endpoint = azure_endpoint or os.environ.get("AZURE_SEARCH_SERVICE_ENDPOINT") or None
+        azure_endpoint = azure_endpoint or os.environ.get("AZURE_AI_SEARCH_ENDPOINT") or None
         if not azure_endpoint:
-            msg = "Please provide an Azure endpoint or set the environment variable AZURE_SEARCH_SERVICE_ENDPOINT."
+            msg = "Please provide an Azure endpoint or set the environment variable AZURE_AI_SEARCH_ENDPOINT."
             raise ValueError(msg)
 
-        api_key = api_key or os.environ.get("AZURE_SEARCH_API_KEY") or None
+        api_key = api_key or os.environ.get("AZURE_AI_SEARCH_API_KEY") or None
 
         self._client = None
         self._index_client = None
