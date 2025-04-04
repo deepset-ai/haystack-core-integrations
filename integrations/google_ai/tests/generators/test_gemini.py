ae41b1aa8fc95b97b05161abf9f04c784ee02fb0
Julian Risch <julian.risch@deepset.ai>
2025-04-04 13:43:28 +0200

    tests: upgrade gemini models (#1617)
    * tests: upgrade gemini models
    
    * Update gemini.py
    
    * Update test_gemini.py
    
    * Update test_gemini.py
    
    * Update gemini.py

--- integrations/google_ai/tests/generators/test_gemini.py
+++ integrations/google_ai/tests/generators/test_gemini.py
@@ -28,7 +28,7 @@ def test_init(monkeypatch):
             safety_settings=safety_settings,
         )
     mock_genai_configure.assert_called_once_with(api_key="test")
-    assert gemini.model_name == "gemini-1.5-flash"
+    assert gemini.model_name == "gemini-2.0-flash"
     assert gemini.generation_config == generation_config
     assert gemini.safety_settings == safety_settings
     assert isinstance(gemini._model, GenerativeModel)
@@ -47,7 +47,7 @@ def test_to_dict(monkeypatch):
     assert gemini.to_dict() == {
         "type": "haystack_integrations.components.generators.google_ai.gemini.GoogleAIGeminiGenerator",
         "init_parameters": {
-            "model": "gemini-1.5-flash",
+            "model": "gemini-2.0-flash",
             "api_key": {"env_vars": ["GOOGLE_API_KEY"], "strict": True, "type": "env_var"},
             "generation_config": None,
             "safety_settings": None,
@@ -77,7 +77,7 @@ def test_to_dict_with_param(monkeypatch):
     assert gemini.to_dict() == {
         "type": "haystack_integrations.components.generators.google_ai.gemini.GoogleAIGeminiGenerator",
         "init_parameters": {
-            "model": "gemini-1.5-flash",
+            "model": "gemini-2.0-flash",
             "api_key": {"env_vars": ["GOOGLE_API_KEY"], "strict": True, "type": "env_var"},
             "generation_config": {
                 "temperature": 0.5,
@@ -101,7 +101,7 @@ def test_from_dict_with_param(monkeypatch):
             {
                 "type": "haystack_integrations.components.generators.google_ai.gemini.GoogleAIGeminiGenerator",
                 "init_parameters": {
-                    "model": "gemini-1.5-flash",
+                    "model": "gemini-2.0-flash",
                     "generation_config": {
                         "temperature": 0.5,
                         "top_p": 0.5,
@@ -116,7 +116,7 @@ def test_from_dict_with_param(monkeypatch):
             }
         )
 
-    assert gemini.model_name == "gemini-1.5-flash"
+    assert gemini.model_name == "gemini-2.0-flash"
     assert gemini.generation_config == GenerationConfig(
         candidate_count=1,
         stop_sequences=["stop"],
@@ -137,7 +137,7 @@ def test_from_dict(monkeypatch):
             {
                 "type": "haystack_integrations.components.generators.google_ai.gemini.GoogleAIGeminiGenerator",
                 "init_parameters": {
-                    "model": "gemini-1.5-flash",
+                    "model": "gemini-2.0-flash",
                     "generation_config": {
                         "temperature": 0.5,
                         "top_p": 0.5,
@@ -152,7 +152,7 @@ def test_from_dict(monkeypatch):
             }
         )
 
-    assert gemini.model_name == "gemini-1.5-flash"
+    assert gemini.model_name == "gemini-2.0-flash"
     assert gemini.generation_config == GenerationConfig(
         candidate_count=1,
         stop_sequences=["stop"],
@@ -167,7 +167,7 @@ def test_from_dict(monkeypatch):
 
 @pytest.mark.skipif(not os.environ.get("GOOGLE_API_KEY", None), reason="GOOGLE_API_KEY env var not set")
 def test_run():
-    gemini = GoogleAIGeminiGenerator(model="gemini-1.5-pro")
+    gemini = GoogleAIGeminiGenerator(model="gemini-2.0-flash")
     res = gemini.run("Tell me something cool")
     assert len(res["replies"]) > 0
 
@@ -180,7 +180,7 @@ def test_run_with_streaming_callback():
         nonlocal streaming_callback_called
         streaming_callback_called = True
 
-    gemini = GoogleAIGeminiGenerator(model="gemini-1.5-pro", streaming_callback=streaming_callback)
+    gemini = GoogleAIGeminiGenerator(model="gemini-2.0-flash", streaming_callback=streaming_callback)
     res = gemini.run("Tell me something cool")
     assert len(res["replies"]) > 0
     assert streaming_callback_called
