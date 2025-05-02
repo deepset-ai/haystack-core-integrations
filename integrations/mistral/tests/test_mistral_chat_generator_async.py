a7955f8a824277bee53ad999aedcc5e24eb1b0c1
Arsenii Shkunkov <shkunkov.as@gmail.com>
2025-05-02 12:45:34 +0200

    Removed duplicate tests, also fixed some async callbacks

--- integrations/mistral/tests/test_mistral_chat_generator_async.py
+++ integrations/mistral/tests/test_mistral_chat_generator_async.py
@@ -4,9 +4,6 @@ from unittest.mock import AsyncMock, patch
 
 import pytest
 import pytz
-from haystack import Pipeline
-from haystack.components.generators.utils import print_streaming_chunk
-from haystack.components.tools import ToolInvoker
 from haystack.dataclasses import (
     ChatMessage,
     ChatRole,
@@ -14,7 +11,6 @@ from haystack.dataclasses import (
     ToolCall,
 )
 from haystack.tools import Tool
-from haystack.utils.auth import Secret
 from openai import AsyncOpenAI, OpenAIError
 from openai.types.chat import ChatCompletion, ChatCompletionMessage
 from openai.types.chat.chat_completion import Choice
@@ -52,36 +48,6 @@ def tools():
     )
 
     return [tool]
-
-
-@pytest.fixture
-def mock_chat_completion():
-    """
-    Mock the OpenAI API completion response and reuse it for tests
-    """
-    with patch("openai.resources.chat.completions.Completions.create") as mock_chat_completion_create:
-        completion = ChatCompletion(
-            id="foo",
-            model="mistral-small-latest",
-            object="chat.completion",
-            choices=[
-                Choice(
-                    finish_reason="stop",
-                    logprobs=None,
-                    index=0,
-                    message=ChatCompletionMessage(content="Hello world!", role="assistant"),
-                )
-            ],
-            created=int(datetime.now(tz=pytz.timezone("UTC")).timestamp()),
-            usage={
-                "prompt_tokens": 57,
-                "completion_tokens": 40,
-                "total_tokens": 97,
-            },
-        )
-
-        mock_chat_completion_create.return_value = completion
-        yield mock_chat_completion_create
 
 
 @pytest.fixture
@@ -127,143 +93,6 @@ class TestMistralChatGeneratorAsync:
         assert component.async_client.base_url == "https://api.mistral.ai/v1/"
         assert not component.generation_kwargs
 
-    def test_init_fail_wo_api_key(self, monkeypatch):
-        monkeypatch.delenv("MISTRAL_API_KEY", raising=False)
-        with pytest.raises(ValueError, match="None of the .* environment variables are set"):
-            MistralChatGenerator()
-
-    def test_init_with_parameters(self):
-        component = MistralChatGenerator(
-            api_key=Secret.from_token("test-api-key"),
-            model="mistral-small",
-            streaming_callback=print_streaming_chunk,
-            api_base_url="test-base-url",
-            generation_kwargs={
-                "max_tokens": 10,
-                "some_test_param": "test-params",
-            },
-        )
-        assert component.client.api_key == "test-api-key"
-        assert component.model == "mistral-small"
-        assert component.streaming_callback is print_streaming_chunk
-        assert component.generation_kwargs == {
-            "max_tokens": 10,
-            "some_test_param": "test-params",
-        }
-
-    def test_to_dict_default(self, monkeypatch):
-        monkeypatch.setenv("MISTRAL_API_KEY", "test-api-key")
-        component = MistralChatGenerator()
-        data = component.to_dict()
-
-        assert (
-            data["type"]
-            == "haystack_integrations.components.generators.mistral.chat.chat_generator.MistralChatGenerator"
-        )
-
-        expected_params = {
-            "api_key": {
-                "env_vars": ["MISTRAL_API_KEY"],
-                "strict": True,
-                "type": "env_var",
-            },
-            "model": "mistral-small-latest",
-            "streaming_callback": None,
-            "api_base_url": "https://api.mistral.ai/v1",
-            "generation_kwargs": {},
-        }
-
-        for key, value in expected_params.items():
-            assert data["init_parameters"][key] == value
-
-    def test_to_dict_with_parameters(self, monkeypatch):
-        monkeypatch.setenv("ENV_VAR", "test-api-key")
-        component = MistralChatGenerator(
-            api_key=Secret.from_env_var("ENV_VAR"),
-            model="mistral-small",
-            streaming_callback=print_streaming_chunk,
-            api_base_url="test-base-url",
-            generation_kwargs={
-                "max_tokens": 10,
-                "some_test_param": "test-params",
-            },
-        )
-        data = component.to_dict()
-
-        assert (
-            data["type"]
-            == "haystack_integrations.components.generators.mistral.chat.chat_generator.MistralChatGenerator"
-        )
-
-        expected_params = {
-            "api_key": {
-                "env_vars": ["ENV_VAR"],
-                "strict": True,
-                "type": "env_var",
-            },
-            "model": "mistral-small",
-            "api_base_url": "test-base-url",
-            "streaming_callback": "haystack.components.generators.utils.print_streaming_chunk",
-            "generation_kwargs": {
-                "max_tokens": 10,
-                "some_test_param": "test-params",
-            },
-        }
-
-        for key, value in expected_params.items():
-            assert data["init_parameters"][key] == value
-
-    def test_from_dict(self, monkeypatch):
-        monkeypatch.setenv("MISTRAL_API_KEY", "fake-api-key")
-        data = {
-            "type": "haystack_integrations.components.generators.mistral.chat.chat_generator.MistralChatGenerator",
-            "init_parameters": {
-                "api_key": {
-                    "env_vars": ["MISTRAL_API_KEY"],
-                    "strict": True,
-                    "type": "env_var",
-                },
-                "model": "mistral-small",
-                "api_base_url": "test-base-url",
-                "streaming_callback": "haystack.components.generators.utils.print_streaming_chunk",
-                "generation_kwargs": {
-                    "max_tokens": 10,
-                    "some_test_param": "test-params",
-                },
-            },
-        }
-        component = MistralChatGenerator.from_dict(data)
-        assert component.model == "mistral-small"
-        assert component.streaming_callback is print_streaming_chunk
-        assert component.api_base_url == "test-base-url"
-        assert component.generation_kwargs == {
-            "max_tokens": 10,
-            "some_test_param": "test-params",
-        }
-        assert component.api_key == Secret.from_env_var("MISTRAL_API_KEY")
-
-    def test_from_dict_fail_wo_env_var(self, monkeypatch):
-        monkeypatch.delenv("MISTRAL_API_KEY", raising=False)
-        data = {
-            "type": "haystack_integrations.components.generators.mistral.chat.chat_generator.MistralChatGenerator",
-            "init_parameters": {
-                "api_key": {
-                    "env_vars": ["MISTRAL_API_KEY"],
-                    "strict": True,
-                    "type": "env_var",
-                },
-                "model": "mistral-small",
-                "api_base_url": "test-base-url",
-                "streaming_callback": "haystack.components.generators.utils.print_streaming_chunk",
-                "generation_kwargs": {
-                    "max_tokens": 10,
-                    "some_test_param": "test-params",
-                },
-            },
-        }
-        with pytest.raises(ValueError, match="None of the .* environment variables are set"):
-            MistralChatGenerator.from_dict(data)
-
     @pytest.mark.asyncio
     async def test_run_async(self, chat_messages, mock_async_chat_completion, monkeypatch):  # noqa: ARG002
         monkeypatch.setenv("MISTRAL_API_KEY", "fake-api-key")
@@ -294,36 +123,6 @@ class TestMistralChatGeneratorAsync:
         assert isinstance(response["replies"], list)
         assert len(response["replies"]) == 1
         assert [isinstance(reply, ChatMessage) for reply in response["replies"]]
-
-    def test_check_abnormal_completions(self, caplog):
-        component = MistralChatGenerator(api_key=Secret.from_token("test-api-key"))
-        messages = [
-            ChatMessage.from_assistant(
-                "",
-                meta={
-                    "finish_reason": ("content_filter" if i % 2 == 0 else "length"),
-                    "index": i,
-                },
-            )
-            for i, _ in enumerate(range(4))
-        ]
-
-        for m in messages:
-            component._check_finish_reason(m.meta)
-
-        # check truncation warning
-        message_template = (
-            "The completion for index {index} has been truncated before reaching a natural stopping point. "
-            "Increase the max_tokens parameter to allow for longer completions."
-        )
-
-        for index in [1, 3]:
-            assert caplog.records[index].message == message_template.format(index=index)
-
-        # check content filter warning
-        message_template = "The completion for index {index} has been truncated due to the content filter."
-        for index in [0, 2]:
-            assert caplog.records[index].message == message_template.format(index=index)
 
     @pytest.mark.skipif(
         not os.environ.get("MISTRAL_API_KEY", None),
@@ -369,11 +168,8 @@ class TestMistralChatGeneratorAsync:
                 self.responses += chunk.content if chunk.content else ""
 
         callback = Callback()
-        component = MistralChatGenerator(streaming_callback=callback)
-        results = await component.run_async(
-            [ChatMessage.from_user("What's the capital of France?")],
-            generation_kwargs={"tool_choice": "any"},
-        )
+        component = MistralChatGenerator(streaming_callback=callback.__call__)
+        results = await component.run_async([ChatMessage.from_user("What's the capital of France?")])
 
         assert len(results["replies"]) == 1
         message: ChatMessage = results["replies"][0]
@@ -459,7 +255,7 @@ class TestMistralChatGeneratorAsync:
     )
     @pytest.mark.integration
     @pytest.mark.asyncio
-    async def test_live_run_with_tools_streaming(self, tools):
+    async def test_live_run_with_tools_streaming_async(self, tools):
         """
         Integration test that the MistralChatGenerator component can run with tools and streaming.
         """
@@ -478,7 +274,7 @@ class TestMistralChatGeneratorAsync:
                     self.tool_calls.extend(chunk.meta["tool_calls"])
 
         callback = Callback()
-        component = MistralChatGenerator(tools=tools, streaming_callback=callback)
+        component = MistralChatGenerator(tools=tools, streaming_callback=callback.__call__)
         results = await component.run_async(
             [ChatMessage.from_user("What's the weather like in Paris?")],
             generation_kwargs={"tool_choice": "any"},
@@ -504,32 +300,3 @@ class TestMistralChatGeneratorAsync:
         assert tool_call.tool_name == "weather"
         assert tool_call.arguments == {"city": "Paris"}
         assert tool_message.meta["finish_reason"] == "tool_calls"
-
-    @pytest.mark.skipif(
-        not os.environ.get("MISTRAL_API_KEY", None),
-        reason="Export an env var called MISTRAL_API_KEY containing the OpenAI API key to run this test.",
-    )
-    @pytest.mark.integration
-    def test_pipeline_with_mistral_chat_generator(self, tools):
-        """
-        Test that the MistralChatGenerator component can be used in a pipeline
-        """
-        pipeline = Pipeline()
-        pipeline.add_component("generator", MistralChatGenerator(tools=tools))
-        pipeline.add_component("tool_invoker", ToolInvoker(tools=tools))
-
-        pipeline.connect("generator", "tool_invoker")
-
-        results = pipeline.run(
-            data={
-                "generator": {
-                    "messages": [ChatMessage.from_user("What's the weather like in Paris?")],
-                    "generation_kwargs": {"tool_choice": "any"},
-                }
-            }
-        )
-
-        assert (
-            "The weather in Paris is sunny and 32°C"
-            == results["tool_invoker"]["tool_messages"][0].tool_call_result.result
-        )
