bf102df08e53d2b72b8b51e5932f025c4520a502
Sebastian Husch Lee <sjrl423@gmail.com>
2025-05-07 12:02:26 +0200

    Test streaming for just cohere

--- integrations/amazon_bedrock/tests/test_chat_generator.py
+++ integrations/amazon_bedrock/tests/test_chat_generator.py
@@ -23,7 +23,8 @@ MODELS_TO_TEST_WITH_TOOLS = [
 ]
 
 # so far we've discovered these models support streaming and tool use
-STREAMING_TOOL_MODELS = ["anthropic.claude-3-5-sonnet-20240620-v1:0", "cohere.command-r-plus-v1:0"]
+# STREAMING_TOOL_MODELS = ["anthropic.claude-3-5-sonnet-20240620-v1:0", "cohere.command-r-plus-v1:0"]
+STREAMING_TOOL_MODELS = ["cohere.command-r-plus-v1:0"]
 
 
 def weather(city: str):
