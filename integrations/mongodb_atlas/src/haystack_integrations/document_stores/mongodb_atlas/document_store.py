5a2bc56027a93f523ca3cfbd4f2fe9e556ab494c
ArzelaAscoIi <37148029+ArzelaAscoIi@users.noreply.github.com>
2025-05-12 11:56:57 +0200

    Update integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/document_store.py
    Co-authored-by: Stefano Fiorucci <stefanofiorucci@gmail.com>

--- integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/document_store.py
+++ integrations/mongodb_atlas/src/haystack_integrations/document_stores/mongodb_atlas/document_store.py
@@ -724,6 +724,9 @@ class MongoDBAtlasDocumentStore:
     def _haystack_doc_to_mongo_doc(self, haystack_doc: Document) -> Dict[str, Any]:
         """
         Parses a Haystack Document to a MongoDB document.
+        
+        :param haystack_doc: the Haystack Document to convert
+        :returns: the MongoDB document in a dictionary representation
         """
         mongo_doc = haystack_doc.to_dict(flatten=False)
         if self.content_field != "content":
