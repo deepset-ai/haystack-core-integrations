39f5b8c8b5433211d1d519ea0ff3678146e7f9dc
Kristof Herrmann <kristof.herrmann@rwth-aachen.de>
2025-05-13 10:33:56 +0200

    fix

--- integrations/mongodb_atlas/tests/test_fulltext_retrieval.py
+++ integrations/mongodb_atlas/tests/test_fulltext_retrieval.py
@@ -121,9 +121,10 @@ class TestFullTextRetrieval:
         assert actual_pipeline[0]["$search"]["compound"]["must"][0]["text"]["path"] == "custom_text"
 
         # Verify the pipeline structure
-        assert len(actual_pipeline) == 4
-        assert "$addFields" in actual_pipeline[2]
-        assert "$project" in actual_pipeline[3]
+        assert len(actual_pipeline) == 5
+        assert "$limit" in actual_pipeline[2]
+        assert "$addFields" in actual_pipeline[3]
+        assert "$project" in actual_pipeline[4]
 
     def test_query_retrieval(self, document_store: MongoDBAtlasDocumentStore):
         results = document_store._fulltext_retrieval(query="fox", top_k=2)
