b8bb2ed382a7fd5fb131e21e422d46ea4972de15
Julian Risch <julian.risch@deepset.ai>
2025-05-27 09:59:14 +0200

    rename context prompt

--- integrations/github/src/haystack_integrations/prompts/github/__init__.py
+++ integrations/github/src/haystack_integrations/prompts/github/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+from .context_prompt import CONTEXT_PROMPT
 from .file_editor_prompt import FILE_EDITOR_PROMPT, FILE_EDITOR_SCHEMA
 from .issue_commenter_prompt import ISSUE_COMMENTER_PROMPT, ISSUE_COMMENTER_SCHEMA
 from .pr_creator_prompt import PR_CREATOR_PROMPT, PR_CREATOR_SCHEMA
@@ -8,6 +9,7 @@ from .repo_viewer_prompt import REPO_VIEWER_PROMPT, REPO_VIEWER_SCHEMA
 from .system_prompt import SYSTEM_PROMPT
 
 __all__ = [
+    "CONTEXT_PROMPT",
     "FILE_EDITOR_PROMPT",
     "FILE_EDITOR_SCHEMA",
     "ISSUE_COMMENTER_PROMPT",
