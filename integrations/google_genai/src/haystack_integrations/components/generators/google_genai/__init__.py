f3d75e3c3c4ffa5443c22aa069b39f60d94cfaf7
Vladimir Blagojevic <dovlex@gmail.com>
2025-06-02 15:42:19 +0200

    feat: Add Google GenAI GoogleGenAIChatGenerator (#1875)
    * Initial work
    
    * Remove utils
    
    * Add async support
    
    * Async test issue
    
    * Simplify async tests
    
    * Linting
    
    * Improve comment
    
    * Linting
    
    * Improve pyproject.toml
    
    * Add new google genai integration to workflow
    
    * Add labeler
    
    * Add pydoc
    
    * Pin deps
    
    * Pin google-genai dep
    
    * Update integrations/google_genai/src/haystack_integrations/components/generators/google_genai/chat/chat_generator.py
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
    
    * Update integrations/google_genai/src/haystack_integrations/components/generators/google_genai/chat/chat_generator.py
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
    
    * PR feedback
    
    * Add system message comment
    
    * Leave only minimal working examples in README
    
    * Update integrations/google_genai/src/haystack_integrations/components/generators/google_genai/chat/chat_generator.py
    
    Co-authored-by: Julian Risch <julian.risch@deepset.ai>
    
    * Update integrations/google_genai/src/haystack_integrations/components/generators/google_genai/chat/chat_generator.py
    
    Co-authored-by: Julian Risch <julian.risch@deepset.ai>
    
    * Linting
    
    ---------
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
    Co-authored-by: Julian Risch <julian.risch@deepset.ai>

---
+++ integrations/google_genai/src/haystack_integrations/components/generators/google_genai/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from .chat.chat_generator import GoogleGenAIChatGenerator
+
+__all__ = ["GoogleGenAIChatGenerator"]
