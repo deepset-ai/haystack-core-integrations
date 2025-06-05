778f702bd3937d9d49274eb9f6745cf158afc3c0
garybadwal <gurpreet071999@gmail.com>
2025-06-05 12:57:10 +0530

    feat: add unit tests for GoogleAIDocumentEmbedder and GoogleAITextEmbedder

---
+++ integrations/google_genai/tests/test_document_embedder.py
@@ -0,0 +1,186 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import random
+from typing import List
+
+import pytest
+
+from haystack import Document
+from haystack_integrations.components.embedders.google_genai import GoogleAIDocumentEmbedder
+from haystack.utils.auth import Secret
+
+def mock_google_response(input: List[str], model: str = "text-embedding-004", **kwargs) -> dict:
+    dict_response = {
+        "embedding": [[random.random() for _ in range(768)] for _ in input],
+        "meta": {
+            "model": model
+        }
+    }
+
+    return dict_response
+
+
+class TestGoogleAIDocumentEmbedder:
+    def test_init_default(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        embedder = GoogleAIDocumentEmbedder()
+        assert embedder.api_key.resolve_value() == "fake-api-key"
+        assert embedder.model == "text-embedding-004"
+        assert embedder.batch_size == 32
+        assert embedder.progress_bar is True
+        assert embedder.meta_fields_to_embed == []
+        assert embedder.embedding_separator == "\n"
+
+    def test_init_with_parameters(self, monkeypatch):
+        embedder = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_token("fake-api-key-2"),
+            model="model",
+            batch_size=64,
+            progress_bar=False,
+            meta_fields_to_embed=["test_field"],
+            embedding_separator=" | ",
+        )
+        assert embedder.api_key.resolve_value() == "fake-api-key-2"
+        assert embedder.model == "model"
+        assert embedder.batch_size == 64
+        assert embedder.progress_bar is False
+        assert embedder.meta_fields_to_embed == ["test_field"]
+        assert embedder.embedding_separator == " | "
+
+    def test_init_with_parameters_and_env_vars(self, monkeypatch):
+        embedder = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_token("fake-api-key-2"),
+            model="model",
+            batch_size=64,
+            progress_bar=False,
+            meta_fields_to_embed=["test_field"],
+            embedding_separator=" | ",
+        )
+        assert embedder.api_key.resolve_value() == "fake-api-key-2"
+        assert embedder.model == "model"
+        assert embedder.batch_size == 64
+        assert embedder.progress_bar is False
+        assert embedder.meta_fields_to_embed == ["test_field"]
+        assert embedder.embedding_separator == " | "
+
+    def test_init_fail_wo_api_key(self, monkeypatch):
+        monkeypatch.delenv("GOOGLE_API_KEY", raising=False)
+        with pytest.raises(ValueError, match="None of the .* environment variables are set"):
+            GoogleAIDocumentEmbedder()
+
+    def test_to_dict(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        component = GoogleAIDocumentEmbedder()
+        data = component.to_dict()
+        assert data == {
+            'type': 'haystack_integrations.components.embedders.google_genai.GoogleAIDocumentEmbedder',
+            'init_parameters': {
+                'model': 'text-embedding-004',
+                'batch_size': 32,
+                'progress_bar': True,
+                'meta_fields_to_embed': [],
+                'embedding_separator': '\n',
+                'api_key': {'type': 'env_var', 'env_vars': ['GOOGLE_API_KEY'], 'strict': True},
+                'config': {'task_type': 'SEMANTIC_SIMILARITY'}
+            }
+        }
+
+    def test_to_dict_with_custom_init_parameters(self, monkeypatch):
+        monkeypatch.setenv("ENV_VAR", "fake-api-key")
+        component = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_env_var("ENV_VAR", strict=False),
+            model="model",
+            batch_size=64,
+            progress_bar=False,
+            meta_fields_to_embed=["test_field"],
+            embedding_separator=" | ",
+        )
+        data = component.to_dict()
+        assert data == {
+            'type': 'haystack_integrations.components.embedders.google_genai.GoogleAIDocumentEmbedder',
+            'init_parameters': {
+                'model': 'model',
+                'batch_size': 64,
+                'progress_bar': False,
+                'meta_fields_to_embed': ['test_field'],
+                'embedding_separator': ' | ',
+                'api_key': {'type': 'env_var', 'env_vars': ['ENV_VAR'], 'strict': False},
+                'config': {'task_type': 'SEMANTIC_SIMILARITY'}
+            }
+        }
+
+    def test_prepare_texts_to_embed_w_metadata(self):
+        documents = [
+            Document(id=f"{i}", content=f"document number {i}:\ncontent", meta={
+                     "meta_field": f"meta_value {i}"})
+            for i in range(5)
+        ]
+
+        embedder = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_token("fake-api-key"), meta_fields_to_embed=["meta_field"], embedding_separator=" | "
+        )
+
+        prepared_texts = embedder._prepare_texts_to_embed(documents)
+
+        assert prepared_texts == {
+            "0": "meta_value 0 | document number 0:\ncontent",
+            "1": "meta_value 1 | document number 1:\ncontent",
+            "2": "meta_value 2 | document number 2:\ncontent",
+            "3": "meta_value 3 | document number 3:\ncontent",
+            "4": "meta_value 4 | document number 4:\ncontent",
+        }
+
+    def test_run_wrong_input_format(self):
+        embedder = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_token("fake-api-key"))
+
+        # wrong formats
+        string_input = "text"
+        list_integers_input = [1, 2, 3]
+
+        with pytest.raises(TypeError, match="GoogleAIDocumentEmbedder expects a list of Documents as input"):
+            embedder.run(documents=string_input)
+
+        with pytest.raises(TypeError, match="GoogleAIDocumentEmbedder expects a list of Documents as input"):
+            embedder.run(documents=list_integers_input)
+
+    def test_run_on_empty_list(self):
+        embedder = GoogleAIDocumentEmbedder(
+            api_key=Secret.from_token("fake-api-key"))
+
+        empty_list_input = []
+        result = embedder.run(documents=empty_list_input)
+
+        assert result["documents"] is not None
+        assert not result["documents"]  # empty list
+
+    @pytest.mark.skipif(os.environ.get("GOOGLE_API_KEY", "") == "", reason="GOOGLE_API_KEY is not set")
+    @pytest.mark.integration
+    def test_run(self):
+        docs = [
+            Document(content="I love cheese", meta={"topic": "Cuisine"}),
+            Document(content="A transformer is a deep learning architecture", meta={
+                     "topic": "ML"}),
+        ]
+
+        model = "text-embedding-004"
+
+        embedder = GoogleAIDocumentEmbedder(model=model, meta_fields_to_embed=[
+                                            "topic"], embedding_separator=" | ")
+
+        result = embedder.run(documents=docs)
+        documents_with_embeddings = result["documents"]
+        assert isinstance(documents_with_embeddings, list)
+        assert len(documents_with_embeddings) == len(docs)
+        for doc in documents_with_embeddings:
+            assert isinstance(doc, Document)
+            assert isinstance(doc.embedding, list)
+            assert len(doc.embedding) == 768
+            assert all(isinstance(x, float) for x in doc.embedding)
+
+        assert "text" in result["meta"]["model"] and "004" in result["meta"]["model"], (
+            "The model name does not contain 'text' and '004'"
+        )
---
+++ integrations/google_genai/tests/test_text_embedder.py
@@ -0,0 +1,152 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+import pytest
+
+from haystack_integrations.components.embedders.google_genai import GoogleAITextEmbedder
+from haystack.utils.auth import Secret
+
+from google.genai import types 
+from google.genai.types import EmbedContentResponse, ContentEmbedding, EmbedContentConfig
+
+
+class TestGoogleAITextEmbedder:
+    def test_init_default(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        embedder = GoogleAITextEmbedder()
+
+        assert embedder._api_key.resolve_value() == "fake-api-key"
+        assert embedder._model_name == "text-embedding-004"
+
+    def test_init_with_parameters(self):
+        embedder = GoogleAITextEmbedder(
+            api_key=Secret.from_token("fake-api-key"),
+            model="model",
+        )
+        assert embedder._api_key.resolve_value() == "fake-api-key"
+        assert embedder._model_name == "model"
+
+    def test_init_with_parameters_and_env_vars(self, monkeypatch):
+        embedder = GoogleAITextEmbedder(
+            api_key=Secret.from_token("fake-api-key"),
+            model="model",
+            config=types.EmbedContentConfig(task_type="SEMANTIC_SIMILARITY")
+        )
+        assert embedder._api_key.resolve_value() == "fake-api-key"
+        assert embedder._model_name == "model"
+        assert embedder._config == types.EmbedContentConfig(
+            task_type="SEMANTIC_SIMILARITY")
+
+    def test_init_fail_wo_api_key(self, monkeypatch):
+        monkeypatch.delenv("GOOGLE_API_KEY", raising=False)
+        with pytest.raises(ValueError, match="None of the .* environment variables are set"):
+            GoogleAITextEmbedder()
+
+    def test_to_dict(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        component = GoogleAITextEmbedder()
+        data = component.to_dict()
+        assert data == {
+            "type": "aystack_integrations.components.embedders.google_genai.GoogleAITextEmbedder",
+            "init_parameters": {
+                'api_key': {'type': 'env_var', 'env_vars': ['GOOGLE_API_KEY'], 'strict': True},
+                "model": "text-embedding-004",
+                'config': {'task_type': 'SEMANTIC_SIMILARITY'}
+            },
+        }
+
+    def test_to_dict_with_custom_init_parameters(self, monkeypatch):
+        monkeypatch.setenv("ENV_VAR", "fake-api-key")
+        component = GoogleAITextEmbedder(
+            api_key=Secret.from_env_var("ENV_VAR", strict=False),
+            model="model",
+            config=types.EmbedContentConfig(
+                task_type="SEMANTIC_SIMILARITY"
+            )
+        )
+        data = component.to_dict()
+        assert data == {
+            'type': 'aystack_integrations.components.embedders.google_genai.GoogleAITextEmbedder',
+            'init_parameters': {
+                'model': 'model',
+                'api_key': {
+                    'type': 'env_var',
+                    'env_vars': ['ENV_VAR'],
+                    'strict': False
+                },
+                'config': {'task_type': 'SEMANTIC_SIMILARITY'}
+            }
+        }
+
+    def test_from_dict(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        data = {
+            "type": "aystack_integrations.components.embedders.google_genai.GoogleAITextEmbedder",
+            "init_parameters": {
+                "api_key": {'type': 'env_var', 'env_vars': ['GOOGLE_API_KEY'], 'strict': True},
+                "model": "text-embedding-004",
+            },
+        }
+        component = GoogleAITextEmbedder.from_dict(data)
+        assert component._api_key.resolve_value() == "fake-api-key"
+        assert component._model_name == "text-embedding-004"
+
+    def test_prepare_input(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+        embedder = GoogleAITextEmbedder()
+
+        contents = "The food was delicious"
+        prepared_input = embedder._prepare_input(contents)
+        assert prepared_input == {
+            "model": "text-embedding-004",
+            "contents": "The food was delicious",
+            "config": EmbedContentConfig(
+                http_options=None,
+                task_type='SEMANTIC_SIMILARITY',
+                title=None,
+                output_dimensionality=None,
+                mime_type=None,
+                auto_truncate=None
+            )
+        }
+
+    def test_prepare_output(self, monkeypatch):
+        monkeypatch.setenv("GOOGLE_API_KEY", "fake-api-key")
+
+        response = EmbedContentResponse(
+            embeddings=[ContentEmbedding(values=[0.1, 0.2, 0.3])],
+        )
+
+        embedder = GoogleAITextEmbedder()
+        result = embedder._prepare_output(result=response)
+        assert result == {
+            "embedding": [0.1, 0.2, 0.3],
+            "meta": {"model": "text-embedding-004"},
+        }
+
+    def test_run_wrong_input_format(self):
+        embedder = GoogleAITextEmbedder(
+            api_key=Secret.from_token("fake-api-key"))
+
+        list_integers_input = [1, 2, 3]
+
+        with pytest.raises(TypeError, match="GoogleAITextEmbedder expects a string as an input"):
+            embedder.run(text=list_integers_input)
+
+    @pytest.mark.skipif(os.environ.get("GOOGLE_API_KEY", "") == "", reason="GOOGLE_API_KEY is not set")
+    @pytest.mark.integration
+    def test_run(self):
+        model = "text-embedding-004"
+
+        embedder = GoogleAITextEmbedder(model=model)
+        result = embedder.run(text="The food was delicious")
+
+        assert len(result["embedding"]) == 768
+        assert all(isinstance(x, float) for x in result["embedding"])
+
+        assert "text" in result["meta"]["model"] and "004" in result["meta"]["model"], (
+            "The model name does not contain 'text' and '004'"
+        )
