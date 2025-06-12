2c4398660ddcc7c6f1932f61b49ea5c66172582b
Sebastian Husch Lee <sjrl423@gmail.com>
2025-06-12 14:50:02 +0200

    Add test running from a pipeline

--- integrations/opensearch/tests/test_open_search_hybrid_retriever.py
+++ integrations/opensearch/tests/test_open_search_hybrid_retriever.py
@@ -3,7 +3,7 @@ from typing import Any, Dict
 from unittest.mock import Mock
 
 import pytest
-from haystack import Document
+from haystack import Document, Pipeline
 from haystack.components.embedders import SentenceTransformersTextEmbedder
 from haystack.core.component import component
 
@@ -207,6 +207,38 @@ class TestOpenSearchHybridRetriever:
             query_embedding=[0.1, 0.2, 0.3],
             filters={"key": "value"},
             top_k=1,
+            custom_query=None,
+            efficient_filtering=False,
+        )
+
+    def test_run_in_pipeline(self, mock_embedder):
+        # mocked document store
+        pipeline = Pipeline()
+        mock_store = Mock(spec=OpenSearchDocumentStore)
+        mock_store._bm25_retrieval.return_value = [Document(content="Test doc BM25")]
+        mock_store._embedding_retrieval.return_value = [Document(content="Test doc Embedding")]
+
+        # use the mocked embedder
+        retriever = OpenSearchHybridRetriever(document_store=mock_store, embedder=mock_embedder)
+        # result = retriever.run(query="test query")
+        pipeline.add_component("retriever", retriever)
+
+        # Should not fail
+        _ = pipeline.run(data={"retriever": {"query": "test query", "filters_bm25": {"param_a": "default"}}})
+
+        mock_store._bm25_retrieval.assert_called_once_with(
+            query="test query",
+            filters={"param_a": "default"},
+            top_k=10,
+            all_terms_must_match=False,
+            fuzziness="AUTO",
+            scale_score=False,
+            custom_query=None,
+        )
+        mock_store._embedding_retrieval.assert_called_once_with(
+            query_embedding=[0.1, 0.2, 0.3],
+            filters={},
+            top_k=10,
             custom_query=None,
             efficient_filtering=False,
         )
