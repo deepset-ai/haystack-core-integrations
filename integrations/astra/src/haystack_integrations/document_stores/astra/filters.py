38f1f1bd701ed17adfe6fc4e07318293f5d9a858
David S. Batista <dsbatista@gmail.com>
2025-12-30 10:53:13 +0100

    feat: adding `delete_by_filter` and `update_by_filter` (#2631)
    * adding delete_by_filter and updated_by_filter
    
    * adding new line to the end of file
    
    * single quotes by double quotes
    
    * formating
    
    * fixing typing
    
    * PR comments/fixes
    
    * updating tests
    
    * fixing client

--- integrations/astra/src/haystack_integrations/document_stores/astra/filters.py
+++ integrations/astra/src/haystack_integrations/document_stores/astra/filters.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023-present Anant Corporation <support@anant.us>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import Any, Optional
 
 from haystack.errors import FilterError
