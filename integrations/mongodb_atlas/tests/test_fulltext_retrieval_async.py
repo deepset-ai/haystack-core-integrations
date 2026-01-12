aadd16e2e95e1395350c2cc1f1ed2f09892f7ef1
anakin87 <stefanofiorucci@gmail.com>
2026-01-12 16:18:03 +0100

    chore!: mongodb_atlas - drop Python 3.9 and use X|Y typing

--- integrations/mongodb_atlas/tests/test_fulltext_retrieval_async.py
+++ integrations/mongodb_atlas/tests/test_fulltext_retrieval_async.py
@@ -4,7 +4,6 @@
 
 import os
 from time import sleep
-from typing import Union
 from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
@@ -116,7 +115,7 @@ class TestFullTextRetrieval:
 
     @pytest.mark.parametrize("query", ["", []])
     async def test_empty_query_raises_value_error_async(
-        self, query: Union[str, list], document_store: MongoDBAtlasDocumentStore
+        self, query: str | list, document_store: MongoDBAtlasDocumentStore
     ):
         with pytest.raises(ValueError):
             await document_store._fulltext_retrieval_async(query=query)
