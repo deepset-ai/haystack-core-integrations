2f60598499ba557dace732ebe89ef73a3c4dffc4
Stefano Fiorucci <stefanofiorucci@gmail.com>
2026-01-13 16:16:46 +0100

    chore!: elasticsearch - drop Python 3.9 and use X|Y typing (#2743)

--- integrations/elasticsearch/src/haystack_integrations/components/retrievers/elasticsearch/embedding_retriever.py
+++ integrations/elasticsearch/src/haystack_integrations/components/retrievers/elasticsearch/embedding_retriever.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
-from typing import Any, Optional, Union
+from typing import Any
 
 from haystack import component, default_from_dict, default_to_dict
 from haystack.dataclasses import Document
@@ -49,10 +49,10 @@ class ElasticsearchEmbeddingRetriever:
         self,
         *,
         document_store: ElasticsearchDocumentStore,
-        filters: Optional[dict[str, Any]] = None,
+        filters: dict[str, Any] | None = None,
         top_k: int = 10,
-        num_candidates: Optional[int] = None,
-        filter_policy: Union[str, FilterPolicy] = FilterPolicy.REPLACE,
+        num_candidates: int | None = None,
+        filter_policy: str | FilterPolicy = FilterPolicy.REPLACE,
     ):
         """
         Create the ElasticsearchEmbeddingRetriever component.
@@ -115,7 +115,7 @@ class ElasticsearchEmbeddingRetriever:
 
     @component.output_types(documents=list[Document])
     def run(
-        self, query_embedding: list[float], filters: Optional[dict[str, Any]] = None, top_k: Optional[int] = None
+        self, query_embedding: list[float], filters: dict[str, Any] | None = None, top_k: int | None = None
     ) -> dict[str, list[Document]]:
         """
         Retrieve documents using a vector similarity metric.
@@ -140,7 +140,7 @@ class ElasticsearchEmbeddingRetriever:
 
     @component.output_types(documents=list[Document])
     async def run_async(
-        self, query_embedding: list[float], filters: Optional[dict[str, Any]] = None, top_k: Optional[int] = None
+        self, query_embedding: list[float], filters: dict[str, Any] | None = None, top_k: int | None = None
     ) -> dict[str, list[Document]]:
         """
         Asynchronously retrieve documents using a vector similarity metric.
