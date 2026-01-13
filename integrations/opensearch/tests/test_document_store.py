78019c317ffa47899d850a6441505626ef858071
David S. Batista <dsbatista@gmail.com>
2026-01-13 15:44:51 +0100

    cleaning up typo

--- integrations/opensearch/tests/test_document_store.py
+++ integrations/opensearch/tests/test_document_store.py
@@ -1,4 +1,4 @@
-gi# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
 
