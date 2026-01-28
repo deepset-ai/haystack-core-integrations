61e25ec35cbf093ae3070405a22ccd66fcb1d973
David S. Batista <dsbatista@gmail.com>
2026-01-28 11:33:20 +0100

    moving opensearch scripts to a separate file

--- integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
+++ integrations/opensearch/src/haystack_integrations/document_stores/opensearch/document_store.py
@@ -18,6 +18,7 @@ from opensearchpy.helpers import async_bulk, bulk
 
 from haystack_integrations.document_stores.opensearch.auth import AsyncAWSAuth, AWSAuth
 from haystack_integrations.document_stores.opensearch.filters import normalize_filters
+from haystack_integrations.document_stores.opensearch.opensearch_scripts import METADATA_SEARCH_JACCARD_SCRIPT
 
 logger = logging.getLogger(__name__)
 
@@ -36,45 +37,6 @@ BM25_SCALING_FACTOR = 8
 
 DEFAULT_SETTINGS = {"index.knn": True}
 DEFAULT_MAX_CHUNK_BYTES = 100 * 1024 * 1024
-
-# OpenSearch/Elasticsearch's scripting language to computes Jaccard similarity for metadata search.
-# (n-gram based similarity, n=3 by default)
-# Between: a metadata field value (doc[params.field]) a query string (params.q)
-METADATA_SEARCH_JACCARD_SCRIPT = """
-String a = doc[params.field].size() != 0 ? doc[params.field].value : null;
-String b = params.q;
-if (a == null || b == null) return 0.0;
-
-a = a.toLowerCase();
-b = b.toLowerCase();
-int n = params.n;
-
-if (a.length() < n || b.length() < n) {
-    return a.equals(b)
-        ? 1.0
-        : (a.contains(b) || b.contains(a) ? 0.5 : 0.0);
-}
-
-Set s1 = new HashSet();
-for (int i = 0; i <= a.length() - n; i++) {
-    s1.add(a.substring(i, i + n));
-}
-
-Set s2 = new HashSet();
-for (int i = 0; i <= b.length() - n; i++) {
-    s2.add(b.substring(i, i + n));
-}
-
-int inter = 0;
-for (def x : s1) {
-    if (s2.contains(x)) inter++;
-}
-
-int union = s1.size() + s2.size() - inter;
-if (union == 0) return 0.0;
-
-return inter / (double) union;
-"""
 
 
 class OpenSearchDocumentStore:
