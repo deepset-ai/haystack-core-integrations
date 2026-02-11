295150a836507bf399b51479a0443c4f844c3412
anakin87 <stefanofiorucci@gmail.com>
2026-02-11 19:16:24 +0100

    rm change

--- integrations/qdrant/src/haystack_integrations/document_stores/qdrant/document_store.py
+++ integrations/qdrant/src/haystack_integrations/document_stores/qdrant/document_store.py
@@ -140,7 +140,6 @@ class QdrantDocumentStore:
             If `":memory:"` - use in-memory Qdrant instance.
             If `str` - use it as a URL parameter.
             If `None` - use default values for host and port.
-            this is legit. this {"not": "legit"}
         :param url:
             Either host or str of `Optional[scheme], host, Optional[port], Optional[prefix]`.
         :param port:
