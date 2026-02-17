9db5ad8827770de8b30607ba9e9955a77f4c509e
Bogdan Kostic <bogdankostic@web.de>
2026-02-17 14:03:25 +0100

    Add LaraDocumentTranslator

---
+++ integrations/lara/src/haystack_integrations/components/translators/lara/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2026-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from haystack_integrations.components.translators.lara.document_translator import (
+    LaraDocumentTranslator,
+)
+
+__all__ = ["LaraDocumentTranslator"]
