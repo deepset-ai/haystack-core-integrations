6827d0f65c239c686bfdcf04e962fe4b5db98615
Julian Risch <julianrisch@gmx.de>
2026-03-02 13:24:55 +0100

    Update integrations/arcadedb/src/haystack_integrations/components/retrievers/arcadedb/embedding_retriever.py

--- integrations/arcadedb/src/haystack_integrations/components/retrievers/arcadedb/embedding_retriever.py
+++ integrations/arcadedb/src/haystack_integrations/components/retrievers/arcadedb/embedding_retriever.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""ArcadeDB Embedding Retriever for Haystack 2.x pipelines."""
-
 from typing import Any
 
 from haystack import Document, component, default_from_dict, default_to_dict
