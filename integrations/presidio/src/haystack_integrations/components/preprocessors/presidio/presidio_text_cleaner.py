efe5b8b89eb1a7f98519a82d53b30a01a358cf39
Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
2026-04-24 13:40:44 +0200

    Update integrations/presidio/src/haystack_integrations/components/preprocessors/presidio/presidio_text_cleaner.py
    Co-authored-by: bogdankostic <bogdankostic@web.de>

--- integrations/presidio/src/haystack_integrations/components/preprocessors/presidio/presidio_text_cleaner.py
+++ integrations/presidio/src/haystack_integrations/components/preprocessors/presidio/presidio_text_cleaner.py
@@ -42,7 +42,7 @@ class PresidioTextCleaner:
     """Mapping from ISO 639-1 language code to the largest available spaCy model for that language.
 
     Used to automatically select an NLP model when `models` is not specified.
-    See https://spacy.io/models for the full list of available spaCy models.
+    See [spaCy documentation](https://spacy.io/models) for the full list of available spaCy models.
     """
 
     def __init__(
