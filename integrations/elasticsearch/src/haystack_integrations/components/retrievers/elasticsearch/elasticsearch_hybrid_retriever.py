79260e9a51681bd38fa17ce44c36ff86c7a07860
Nitro963 <adnankattan9@gmail.com>
2026-05-31 13:35:10 +0300

    Fix: use AsyncPipeline with super_component

--- integrations/elasticsearch/src/haystack_integrations/components/retrievers/elasticsearch/elasticsearch_hybrid_retriever.py
+++ integrations/elasticsearch/src/haystack_integrations/components/retrievers/elasticsearch/elasticsearch_hybrid_retriever.py
@@ -4,7 +4,7 @@
 
 from typing import TYPE_CHECKING, Any
 
-from haystack import Document, Pipeline, default_from_dict, default_to_dict, logging, super_component
+from haystack import Document, AsyncPipeline, default_from_dict, default_to_dict, logging, super_component
 from haystack.components.embedders.types import TextEmbedder
 from haystack.components.joiners import DocumentJoiner
 from haystack.components.joiners.document_joiner import JoinMode
@@ -257,7 +257,7 @@ class ElasticsearchHybridRetriever:
         bm25_retriever = ElasticsearchBM25Retriever(**data["bm25_retriever"])
         document_joiner = DocumentJoiner(**data["document_joiner"])
 
-        hybrid_retrieval = Pipeline()
+        hybrid_retrieval = AsyncPipeline()
         hybrid_retrieval.add_component("text_embedder", self.embedder)
         hybrid_retrieval.add_component("embedding_retriever", embedding_retriever)
         hybrid_retrieval.add_component("bm25_retriever", bm25_retriever)
--- integrations/opensearch/src/haystack_integrations/components/retrievers/opensearch/open_search_hybrid_retriever.py
+++ integrations/opensearch/src/haystack_integrations/components/retrievers/opensearch/open_search_hybrid_retriever.py
@@ -4,7 +4,7 @@
 
 from typing import TYPE_CHECKING, Any
 
-from haystack import Document, Pipeline, default_from_dict, default_to_dict, logging, super_component
+from haystack import Document, AsyncPipeline, default_from_dict, default_to_dict, logging, super_component
 from haystack.components.embedders.types import TextEmbedder
 from haystack.components.joiners import DocumentJoiner
 from haystack.components.joiners.document_joiner import JoinMode
@@ -266,7 +266,7 @@ class OpenSearchHybridRetriever:
         bm25_retriever = OpenSearchBM25Retriever(**data["bm25_retriever"])
         document_joiner = DocumentJoiner(**data["document_joiner"])
 
-        hybrid_retrieval = Pipeline()
+        hybrid_retrieval = AsyncPipeline()
         hybrid_retrieval.add_component("text_embedder", self.embedder)
         hybrid_retrieval.add_component("embedding_retriever", embedding_retriever)
         hybrid_retrieval.add_component("bm25_retriever", bm25_retriever)
