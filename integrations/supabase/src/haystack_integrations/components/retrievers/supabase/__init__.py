b8b0ef3a1f93cd0b618bae2d785a9139fb279e02
David S. Batista <dsbatista@gmail.com>
2026-06-01 14:27:13 +0200

    adding missed file

--- integrations/supabase/src/haystack_integrations/components/retrievers/supabase/__init__.py
+++ integrations/supabase/src/haystack_integrations/components/retrievers/supabase/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai>
 #
 # SPDX-License-Identifier: Apache-2.0
+
 from .embedding_retriever import SupabasePgvectorEmbeddingRetriever
 from .groonga_bm25_retriever import SupabaseGroongaBM25Retriever
 from .keyword_retriever import SupabasePgvectorKeywordRetriever
