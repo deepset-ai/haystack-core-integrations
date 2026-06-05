c5bc69d5529c730b673cf351ff76cfacbb213ff5
anakin87 <stefanofiorucci@gmail.com>
2026-06-05 12:22:10 +0200

    feat: move HuggingFaceTEIRanker from Haystack

---
+++ integrations/huggingface_api/src/haystack_integrations/components/rankers/huggingface_api/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .ranker import HuggingFaceTEIRanker, TruncationDirection
+
+__all__ = ["HuggingFaceTEIRanker", "TruncationDirection"]
