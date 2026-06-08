d6f0133277514cb2482318d5506549a84c62e214
Stefano Fiorucci <stefanofiorucci@gmail.com>
2026-06-08 17:50:54 +0200

    feat: move Transformers components from Haystack (#3409)

---
+++ integrations/transformers/src/haystack_integrations/components/classifiers/transformers/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .zero_shot_document_classifier import TransformersZeroShotDocumentClassifier
+
+__all__ = ["TransformersZeroShotDocumentClassifier"]
---
+++ integrations/transformers/src/haystack_integrations/components/readers/transformers/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .extractive_reader import TransformersExtractiveReader
+
+__all__ = ["TransformersExtractiveReader"]
