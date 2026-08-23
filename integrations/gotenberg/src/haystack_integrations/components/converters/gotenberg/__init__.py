3e3dc434417ce7fed8f3f995dc3b2ce8c6621528
Max Swain <89113255+maxdswain@users.noreply.github.com>
2026-08-23 18:51:06 +0100

    feat: add Gotenberg converter integration

---
+++ integrations/gotenberg/src/haystack_integrations/components/converters/gotenberg/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022-present deepset GmbH <info@deepset.ai>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from haystack_integrations.components.converters.gotenberg.converter import ConversionType, GotenbergFileConverter
+
+__all__ = ["ConversionType", "GotenbergFileConverter"]
