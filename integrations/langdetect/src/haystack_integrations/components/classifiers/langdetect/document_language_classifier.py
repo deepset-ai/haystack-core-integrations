1cee9483a92c96f3e75b5d5a4c1e0bade310d00b
mustafaabadshah <mianmustafa926@gmail.com>
2026-09-17 00:09:29 +0500

    fix: validate all documents in DocumentLanguageClassifier.run input check

--- integrations/langdetect/src/haystack_integrations/components/classifiers/langdetect/document_language_classifier.py
+++ integrations/langdetect/src/haystack_integrations/components/classifiers/langdetect/document_language_classifier.py
@@ -85,7 +85,7 @@ class DocumentLanguageClassifier:
 
         :raises TypeError: if the input is not a list of Documents.
         """
-        if not isinstance(documents, list) or (documents and not isinstance(documents[0], Document)):
+        if not isinstance(documents, list) or not all(isinstance(doc, Document) for doc in documents):
             msg = (
                 "DocumentLanguageClassifier expects a list of Document as input. "
                 "In case you want to classify and route a text, please use the TextLanguageRouter."
